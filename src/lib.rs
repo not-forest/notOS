6c8bd90b73066e02c8736cd8d372e5697d57fb91
Notforest <notforest69@gmail.com>
2023-07-09 05:17:43 +0200

    Testing colors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,29 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
 
 mod kernel_components {
     pub mod vga_buffer;
 }
 
 use core::panic::PanicInfo;
+use kernel_components::vga_buffer::Color;
 
 #[no_mangle]
 #[allow(unreachable_code)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello world! {}", "OS");
+    println!("I can write a lot.");
+    println!("A lot of {}", "colorful");
+    println!(Color::MAGENTA; "Trully colorful");
+    println!(Color::GREEN; Color::DARKGRAY; "For sure");
+    println!(Color::BLACK; Color::WHITE; "Hello world! {}", "The world is in colors!");
+    panic!("Some error oh no!");
 
     loop {}
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    println!(Color::RED; "{}", info);
     loop {}
 }
