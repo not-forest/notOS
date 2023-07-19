172e93c14bad43a55e466463fe649afa06f62c03
Notforest <notforest69@gmail.com>
2023-07-19 18:00:06 +0200

    Changed the structure of main file. The testing part were moved to library.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks, used_with_arg)]
-#![test_runner(crate::test_runner)]
+#![test_runner(notOS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 #[link(name = "bootloader")]
@@ -18,69 +18,19 @@ static HEADER_START_FUNC: unsafe extern "C" fn() = header_start;
 #[used(linker)]
 static HEADER_END_FUNC: unsafe extern "C" fn() = header_end;
 
-
-pub mod kernel_components {
-    pub mod vga_buffer;
-}
-
-use core::panic::PanicInfo;
-use kernel_components::vga_buffer::Color;
+use notOS::{println, print, kernel_components::vga_buffer::Color};
 
 #[no_mangle]
 pub extern "C" fn _start() {
-
     #[cfg(test)]
     test_main();
-
 
     main();
 }
 
+#[allow(dead_code, unreachable_code)]
 fn main() -> ! {
     println!(Color::BLUE; "Hello there");
-
+    panic!("I would panic!");
     loop {}
-}
-
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    #[cfg(test)]
-    println!(Color::RED; "[failed]");
-
-    println!(Color::RED; "{}", info);
-    loop {}
-}
-
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T where T: Fn(), {
-    fn run(&self) {
-        print!("{}...    ", core::any::type_name::<T>());
-        self();
-        println!(Color::GREEN; "[ok]");
-    }
-}
-
-#[no_mangle]
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) -> ! {
-    println!(Color::LIGHTBLUE; "Running {} tests:", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    loop {}
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-#[test_case]
-fn trivial_assertion2() {
-    assert_eq!(1, 2);
 }
