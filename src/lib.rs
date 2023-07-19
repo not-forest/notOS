95d46054dfa8d8a2d010915d0bb7d791064e3577
Notforest <notforest69@gmail.com>
2023-07-19 18:02:58 +0200

    Formed a library to create more structured kernel. Integration tests now have access with the main library.

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks, used_with_arg)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+pub mod kernel_components {
+    pub mod vga_buffer;
+}
+
+use core::panic::PanicInfo;
+use kernel_components::vga_buffer::Color;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    #[cfg(test)]
+    println!(Color::RED; "[failed]");
+    println!(Color::RED; "{}", info);
+    
+    loop {}
+}
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T where T: Fn(), {
+    fn run(&self) {
+        print!("{}...    ", core::any::type_name::<T>());
+        self();
+        println!(Color::GREEN; "[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Fn()]) -> ! {
+    println!(Color::LIGHTBLUE; "Running {} tests:", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    loop {}
+}
