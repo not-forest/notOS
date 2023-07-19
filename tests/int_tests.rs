412b2fa9a614ac56dc5dd0fe66ddfaa3a59a0e4f
Notforest <notforest69@gmail.com>
2023-07-19 18:03:45 +0200

    Added intergration tests for future debuging.

---
+++ tests/int_tests.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks, used_with_arg)]
+#![test_runner(notOS::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[link(name = "bootloader")]
+extern "C" {
+    fn initiate();
+    fn header_start();
+    fn header_end();
+}
+
+#[used]
+static INITIATE_FUNC: unsafe extern "C" fn() = initiate;
+#[used(linker)]
+static HEADER_START_FUNC: unsafe extern "C" fn() = header_start;
+#[used(linker)]
+static HEADER_END_FUNC: unsafe extern "C" fn() = header_end;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test_main();
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+}
+}
