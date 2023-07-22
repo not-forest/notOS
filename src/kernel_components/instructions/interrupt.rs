171982895cee9eb213ff7c0dec2b6a38e1e91474
Notforest <notforest69@gmail.com>
2023-07-22 02:01:54 +0200

    This will be the interrupt handler and wrappers for assembly commands. This was created now  for temporal hlt() function for mutex.

---
+++ src/kernel_components/instructions/interrupt.rs
@@ -0,0 +1,21 @@
+// Interrupts and other procedures to the CPU
+
+use core::arch::asm;
+
+// Enable interrupts
+#[inline(always)]
+pub fn enable() {
+    unsafe { asm!("sti", options(nomem, nostack)); }
+}
+
+// Disable interrupts
+#[inline(always)]
+pub fn disable() {
+    unsafe { asm!("cli", options(nomem, nostack)); }
+}
+
+// The hlt function wrapper
+#[inline(always)]
+pub fn hlt() {
+    unsafe { asm!("hlt", options(nomem, nostack)); }
+}
