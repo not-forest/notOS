91a1cda27e5d29e270fd0f7e906f3f0b2dbe1e06
Notforest <notforest69@gmail.com>
2023-07-28 22:57:27 +0200

    Added new modules.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
+// Main library space. It acts as a entry point of the crate.
+// Every single outer files will be inside the kernel_components dir.
+// Every single macro can be accessed within this crate. The main components will be also accessed from here.
+
 #![no_std]
 #![cfg_attr(test, no_main)]
-#![feature(custom_test_frameworks, used_with_arg, error_in_core)]
+#![allow(incomplete_features)]
+#![feature(custom_test_frameworks, used_with_arg, error_in_core, ptr_metadata, generic_const_exprs)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+// Main entry point for outer structures and objects
 pub mod kernel_components {
     pub mod vga_buffer;
     pub mod error;
+
+    pub mod structures {
+        pub mod iternum;
+
+        pub use iternum::IternumTrait;
+    }
 
     pub mod instructions {
         pub mod interrupt;
@@ -22,13 +34,18 @@ pub mod kernel_components {
 
     pub mod memory {
         pub mod memory_module;
+        pub mod tags;
+
+        pub use memory_module::{InfoPointer, BootInfoHeader};
     }
 
 }
 
 use core::panic::PanicInfo;
-use kernel_components::vga_buffer::Color;
+pub use kernel_components::vga_buffer::Color;
+pub use kernel_components::error::*;
 
+// This function will be called on fatal errors in the system.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     #[cfg(test)]
@@ -37,6 +54,8 @@ fn panic(info: &PanicInfo) -> ! {
     
     loop {}
 }
+
+// Custom trait for tests. It is only used when testing and do not affect overall performance.
 pub trait Testable {
     fn run(&self) -> ();
 }
