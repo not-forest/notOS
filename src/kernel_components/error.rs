1da06c9ebf2d5af95efa5ff7353f5fff8c6b1e94
Notforest <notforest69@gmail.com>
2023-07-28 22:58:56 +0200

    Added MbiLoadError.

--- src/kernel_components/error.rs
+++ src/kernel_components/error.rs
@@ -12,4 +12,28 @@ impl Display for PoisonError {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum MbiLoadError {
+    IllegalAddress,
+    IllegalTotalSize(u32),
+    NoEndTag,
+}
+
+impl Display for MbiLoadError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::IllegalAddress => {
+                write!(f, "Illegal address encountered during MBI load")
+            }
+            Self::IllegalTotalSize(size) => {
+                write!(f, "Illegal total size encountered during MBI load: {}", size)
+            }
+            Self::NoEndTag => {
+                write!(f, "No end tag found during MBI load")
+            }
+        }
+    }
+}
+
+impl Error for MbiLoadError {}
 impl Error for PoisonError {}
