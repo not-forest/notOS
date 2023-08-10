537f6ef2d59a77b96d5972f8123d16c008f8ca14
Notforest <notforest69@gmail.com>
2023-08-10 19:22:08 +0200

    Fixed some wrong info about Iternum trait.

--- src/kernel_components/structures/iternum.rs
+++ src/kernel_components/structures/iternum.rs
@@ -1,11 +1,12 @@
-// The iterable two-side enum trait.
+// The iterable two-side enum trait. The trait itself must have Clone, Copy, PartialEq, Eq derives,
+// to work correctly.
 
 pub trait IternumTrait: Sized {
     // The overall size of enum.
     const SIZE: usize;
     // This function creates an array of variants where each item is a reference to a variant
     fn iter() -> [Self; Self::SIZE];
-    // Returns an index of the variant inside the enum. If it does not exist returns -1.
+    // Returns an index of the variant inside the enum. If it does not exist returns usize::MAX.
     fn get_index(variant: Self) -> usize;
     // Returns the variant that is located at corresponding index.
     fn get_variant(index: usize) -> Self;
