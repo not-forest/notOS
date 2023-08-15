fafc9a5fe3d8f4f10315f336a0438a89f2985b41
Notforest <notforest69@gmail.com>
2023-08-15 10:54:39 +0200

    Added MemoryAreaIter and implemented an Iterator for it.

--- src/kernel_components/memory/memory_map.rs
+++ src/kernel_components/memory/memory_map.rs
@@ -2,6 +2,8 @@ use core::fmt::{Debug, Formatter};
 use core::marker::PhantomData;
 use core::mem;
 use core::ops::Deref;
+/// A memory map tag and it's corresponding structures and methods.
+
 use proc_macros::Iternum;
 use crate::{Vec, AsBytes};
 use crate::kernel_components::structures::{
@@ -15,16 +17,16 @@ const METADATA_SIZE: usize = mem::size_of::<TagTypeId>() + 3 * mem::size_of::<u3
 
 impl AsBytes for u32 {}
 
-// This tag provides an initial host memory map (legacy boot, not UEFI).
-//
-// The map provided is guaranteed to list all standard RAM that should be
-// available for normal use. This type however includes the regions occupied
-// by kernel, mbi, segments and modules. Kernel must take care not to
-// overwrite these regions.
-//
-// This tag may not be provided by some boot loaders on EFI platforms if EFI
-// boot services are enabled and available for the loaded image (The EFI boot
-// services tag may exist in the Multiboot2 boot information structure).
+/// This tag provides an initial host memory map (legacy boot, not UEFI).
+///
+/// The map provided is guaranteed to list all standard RAM that should be
+/// available for normal use. This type however includes the regions occupied
+/// by kernel, mbi, segments and modules. Kernel must take care not to
+/// overwrite these regions.
+///
+/// This tag may not be provided by some boot loaders on EFI platforms if EFI
+/// boot services are enabled and available for the loaded image (The EFI boot
+/// services tag may exist in the Multiboot2 boot information structure).
 #[derive(Debug, PartialEq, Eq)]
 #[repr(C)]
 pub struct MemoryMapTag {
@@ -36,6 +38,7 @@ pub struct MemoryMapTag {
 }
 
 impl MemoryMapTag {
+    /// Creates a new memory map tag.
     pub fn new(areas: &[MemoryArea]) -> BoxedDst<Self> {
         let entry_size: u32 = mem::size_of::<MemoryArea>().try_into().unwrap();
         let entry_version: u32 = 0;
@@ -48,17 +51,17 @@ impl MemoryMapTag {
         BoxedDst::new(bytes.as_bytes().deref())
     }
 
-    // Returns the entry size.
+    /// Returns the entry size.
     pub fn entry_size(&self) -> u32 {
         self.entry_size
     }
 
-    // Returns the entry version.
+    /// Returns the entry version.
     pub fn entry_version(&self) -> u32 {
         self.entry_version
     }
 
-    // Return the slice with all memory areas.
+    /// Return the slice with all memory areas.
     pub fn memory_areas(&self) -> &[MemoryArea] {
         &self.areas
     }
@@ -75,7 +78,7 @@ impl TagTrait for MemoryMapTag {
     }
 }
 
-// A memory area entry descriptor.
+/// A memory area entry descriptor.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(C)]
 pub struct MemoryArea {
@@ -89,7 +92,7 @@ pub struct MemoryArea {
 impl AsBytes for MemoryArea {}
 
 impl MemoryArea {
-    // Create a new MemoryArea.
+    /// Create a new MemoryArea.
     pub fn new(base_addr: u64, length: u64, typ: impl Into<MemoryAreaTypeId>) -> Self {
         Self {
             base_addr,
@@ -99,22 +102,22 @@ impl MemoryArea {
         }
     }
 
-    // The start address of the memory region.
+    /// The start address of the memory region.
     pub fn start_address(&self) -> u64 {
         self.base_addr
     }
 
-    // The end address of the memory region.
+    /// The end address of the memory region.
     pub fn end_address(&self) -> u64 {
         self.base_addr + self.length
     }
 
-    // The size, in bytes, of the memory region.
+    /// The size, in bytes, of the memory region.
     pub fn size(&self) -> u64 {
         self.length
     }
 
-    // The type of the memory region.
+    /// The type of the memory region.
     pub fn typ(&self) -> MemoryAreaTypeId {
         self.typ
     }
@@ -154,25 +157,46 @@ impl Debug for MemoryAreaTypeId {
     }
 }
 
-// Abstraction over defined memory types for the memory map as well as custom
-// ones. Types 1 to 5 are defined in the Multiboot2 spec and correspond to the
-// entry types of e820 memory maps.
+/// Abstraction over defined memory types for the memory map as well as custom
+/// ones. Types 1 to 5 are defined in the Multiboot2 spec and correspond to the
+/// entry types of e820 memory maps.
 #[derive(Iternum, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum MemoryAreaType {
-    // Available memory free to be used by the OS.
+    /// Available memory free to be used by the OS.
     Available,
-    // A reserved area that must not be used.
+    /// A reserved area that must not be used.
     Reserved,
-    // Usable memory holding ACPI information.
+    /// Usable memory holding ACPI information.
     AcpiAvailable,
-    // Reserved memory which needs to be preserved on hibernation.
-    // Also called NVS in spec, which stands for "Non-Volatile Sleep/Storage",
-    // which is part of ACPI specification.
+    /// Reserved memory which needs to be preserved on hibernation.
+    /// Also called NVS in spec, which stands for "Non-Volatile Sleep/Storage",
+    /// which is part of ACPI specification.
     ReservedHibernate,
-    // Memory which is occupied by defective RAM modules.
+    /// Memory which is occupied by defective RAM modules.
     Defective,
-    // Custom memory map type.
+    /// Custom memory map type.
     Custom(u32),
+}
+
+#[derive(Clone, Debug)]
+pub struct MemoryAreaIter {
+    current_area: u64,
+    last_area: u64,
+    entry_size: u32,
+    phantom: PhantomData<MemoryArea>
+}
+
+impl Iterator for MemoryAreaIter {
+    type Item = MemoryArea;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_area > self.last_area {
+            None
+        } else {
+            let area = unsafe { *(self.current_area as *const MemoryArea) };
+            self.current_area += self.entry_size as u64;
+            Some(area)
+        }
+    }
 }
 
 impl From<MemoryAreaTypeId> for MemoryAreaType {
