b524acbf06feab53e93ccaa707d68abe554109d7
Notforest <notforest69@gmail.com>
2023-08-21 13:39:12 +0200

    Allocation every frame in the memory given to qemu.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![allow(non_snake_case)]
 #![feature(custom_test_frameworks, used_with_arg)]
 #![test_runner(notOS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -21,7 +22,7 @@ static HEADER_END_FUNC: unsafe extern "C" fn() = header_end;
 /// This is the main binary (kernel) space. As the library will build in, more new features will be added further.
 use notOS::{println, print, 
     kernel_components::
-        memory::{InfoPointer, BootInfoHeader},              
+        memory::{InfoPointer, BootInfoHeader, AreaFrameAllocator, frames::FrameAlloc},            
     Color,
 };
 
@@ -34,18 +35,40 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
         let elf_sections_tag = boot_info.elf_sections_tag()
             .expect("Elf-sections tag required.");
 
-        
+        let kernel_start = boot_info.kstart();
+        let kernel_end = boot_info.kend();
+        let multiboot_start = _multiboot_information_address;
+        let multiboot_end = multiboot_start + ( boot_info.total() as usize );
+
+        /*
         println!("Memory Areas:");
         for area in memory_map_tag.memory_areas() {
             println!(Color::GREEN; "      start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
         }
 
         println!("Kernel Sections:");
-        for section in elf_sections_tag {
+        for (num, section) in elf_sections_tag.enumerate() {
             let section_inner = section.get();
-            println!(Color::LIGHTGREEN; "      addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}", section_inner.addr(), section_inner.size(), section_inner.flags());
+            println!(Color::LIGHTGREEN; "      addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}, number: {}", section_inner.addr(), section_inner.size(), section_inner.flags(), num);
         }
-    }
+        */
+
+        let mut frame_allocator = AreaFrameAllocator::new(
+            kernel_start as usize, 
+            kernel_end as usize, 
+            multiboot_start, 
+            multiboot_end,
+            memory_map_tag.memory_map_iter(),
+        );
+
+        for i in 0.. {
+            if let None = frame_allocator.alloc() {
+                println!(Color::MAGENTA; "Allocated {} frames", i);
+                break;
+            }
+        }
+    }    
+    
 
     #[cfg(test)]
     test_main();
