c5540209b1f2b1990b317993ed6aff2fa17522e6
Notforest <notforest69@gmail.com>
2023-09-06 00:07:07 +0200

    The bitflags! macro now has more features, including multiple sets of structures as an input. BitXor and Not are added.

--- src/kernel_components/structures/bitflags.rs
+++ src/kernel_components/structures/bitflags.rs
@@ -20,6 +20,42 @@ impl<T: Copy + Clone> BitNode<T> where
     }
 }
 
+impl<T> From<T> for BitNode<T> {
+    fn from(value: T) -> Self {
+        BitNode(value)
+    }
+}
+
+impl From<BitNode<u16>> for u16 {
+    fn from(value: BitNode<u16>) -> Self {
+        value.0
+    }
+}
+
+impl From<BitNode<u32>> for u32 {
+    fn from(value: BitNode<u32>) -> Self {
+        value.0
+    }
+}
+
+impl From<BitNode<u64>> for u64 {
+    fn from(value: BitNode<u64>) -> Self {
+        value.0
+    }
+}
+
+impl From<BitNode<u128>> for u128 {
+    fn from(value: BitNode<u128>) -> Self {
+        value.0
+    }
+}
+
+impl From<BitNode<usize>> for usize {
+    fn from(value: BitNode<usize>) -> Self {
+        value.0
+    }
+}
+
 impl<T> Deref for BitNode<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
@@ -90,14 +126,80 @@ macro_rules! bitflags {
                 $(#[$flag_meta:meta])*
                 const $flag:ident = $bit:expr,
             )*
+        };
+    ) => {
+        bitflags! {
+            $(#[$meta])*
+            pub struct $name: $underlying {
+                $(
+                    $(#[$flag_meta])*
+                    const $flag = $bit,
+                )*
+            }
+        }
+    };
+
+    (
+        $(
+            $(#[$meta:meta])*
+            pub struct $name:ident: $underlying:ty {
+                $(
+                    $(#[$flag_meta:meta])*
+                    const $flag:ident = $bit:expr,
+                )*
+            };
+        )*
+    ) => {
+        $(
+            bitflags! {
+                $(#[$meta])*
+                pub struct $name: $underlying {
+                    $(
+                        $(#[$flag_meta])*
+                        const $flag = $bit,
+                    )*
+                }
+            }
+        )*
+    };
+    
+    (
+        $(#[$meta:meta])*
+        pub struct $name:ident: $underlying:ty {
+            $(
+                $(#[$flag_meta:meta])*
+                const $flag:ident = $bit:expr,
+            )*
         }
     ) => {
-        use core::ops::{BitAnd, BitOr, BitXor, Not};
-        use crate::kernel_components::structures::BitNode;
-        use $name::*;
+        crate::_inner_bitflags! {
+            $(#[$meta])*
+            pub struct $name: $underlying {
+                $(
+                    $(#[$flag_meta])*
+                    const $flag = $bit,
+                )*
+            }
+        }
+    };
 
+    () => ();
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! _inner_bitflags {
+    (
+        $(#[$meta:meta])*
+        pub struct $name:ident: $underlying:ty {
+            $(
+                $(#[$flag_meta:meta])*
+                const $flag:ident = $bit:expr,
+            )*
+        }
+    ) => {
         $(#[$meta])*
-        #[derive(Debug, Hash)]
+        #[allow(non_camel_case_types)]
         #[repr($underlying)]
         pub enum $name {
             $(
@@ -122,70 +224,72 @@ macro_rules! bitflags {
                 Self::ALL
             }
 
-            pub fn from_bits_truncate(bits: $underlying) -> BitNode<$underlying> {
-                (Custom(bits) & Self::all()).into()
+            pub fn from_bits_truncate(bits: $underlying) -> crate::kernel_components::structures::BitNode<$underlying> {
+                ($name::Custom(bits) & Self::all()).into()
             }
 
-            pub fn as_node(&self) -> BitNode<$underlying> {
-                BitNode((*self as $name).into())
-            }
-        }
-
-        impl From<$underlying> for BitNode<$underlying> {
-            fn from(value: $underlying) -> Self {
-                BitNode(value)
-            }
-        }
-        
-        impl From<BitNode<$underlying>> for $underlying {
-            fn from(value: BitNode<$underlying>) -> Self {
-                value.0
+            pub fn as_node(&self) -> crate::kernel_components::structures::BitNode<$underlying> {
+                crate::kernel_components::structures::BitNode((*self as $name).into())
             }
         }
 
         impl From<$underlying> for $name {
             fn from(value: $underlying) -> Self {
-                Custom(value)
+                $name::Custom(value)
             }
         }
 
         impl From<$name> for $underlying {
             fn from(value: $name) -> Self {
                 match value {
-                    Custom(n) => n,
-                    ALL => <$underlying>::MAX,
-                    EMPTY => 0 as $underlying,
+                    $name::Custom(n) => n,
+                    $name::ALL => <$underlying>::MAX,
+                    $name::EMPTY => 0 as $underlying,
                     $(
                         $(#[$flag_meta])*
-                        $flag => $bit,
+                        $name::$flag => $bit,
                     )*
                 }
             }
         }
 
-        impl From<$name> for BitNode<$underlying> {
+        impl From<$name> for crate::kernel_components::structures::BitNode<$underlying> {
             fn from(value: $name) -> Self {
-                BitNode(<$underlying>::from(value))
+                crate::kernel_components::structures::BitNode(<$underlying>::from(value))
             }
         }
 
-        impl From<BitNode<$underlying>> for $name {
-            fn from(value: BitNode<$underlying>) -> Self {
-                Custom(value.0)
+        impl From<crate::kernel_components::structures::BitNode<$underlying>> for $name {
+            fn from(value: crate::kernel_components::structures::BitNode<$underlying>) -> Self {
+                $name::Custom(value.0)
             }
         }
 
-        impl BitAnd for $name {
+        impl core::ops::BitAnd for $name {
             type Output = Self;
             fn bitand(self, rhs: Self) -> Self::Output {
-                Custom(<$underlying>::from(self) & <$underlying>::from(rhs))
+                $name::Custom(<$underlying>::from(self) & <$underlying>::from(rhs))
             }
         }
 
-        impl BitOr for $name {
+        impl core::ops::BitOr for $name {
             type Output = Self;
             fn bitor(self, rhs: Self) -> Self::Output {
-                Custom(<$underlying>::from(self) | <$underlying>::from(rhs))
+                $name::Custom(<$underlying>::from(self) | <$underlying>::from(rhs))
+            }
+        }
+
+        impl core::ops::BitXor for $name {
+            type Output = Self;
+            fn bitxor(self, rhs: Self) -> Self::Output {
+                $name::Custom(<$underlying>::from(self) ^ <$underlying>::from(rhs))
+            }
+        }
+
+        impl core::ops::Not for $name {
+            type Output = Self;
+            fn not(self) -> Self::Output {
+                $name::Custom(!<$underlying>::from(self))
             }
         }
     };
