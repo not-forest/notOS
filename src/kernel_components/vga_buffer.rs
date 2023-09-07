84f4cd92bf3bdb0d54db14616934c4a0f546961b
Notforest <notforest69@gmail.com>
2023-09-07 19:35:23 +0200

    Minor macro changes.

--- src/kernel_components/vga_buffer.rs
+++ src/kernel_components/vga_buffer.rs
@@ -230,9 +230,9 @@ macro_rules! print {
 /// '''
 #[macro_export]
 macro_rules! println {
-    () => (print!("\n"));
-    ($fmt:expr) => (print!(concat!($fmt, "\n")));
-    ($fmt:expr, $($arg:tt)*) => (print!(concat!($fmt, "\n"), $($arg)*));
+    () => (crate::print!("\n"));
+    ($fmt:expr) => (crate::print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => (crate::print!(concat!($fmt, "\n"), $($arg)*));
     ($fr:expr; $fmt:expr) => {
         $crate::kernel_components::vga_buffer::_coloring($fr, None);
         println!($fmt);
