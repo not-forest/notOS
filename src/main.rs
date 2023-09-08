a3345ad0a780f1055374ff64aa7782c508aa645d
Notforest <notforest69@gmail.com>
2023-09-08 23:18:21 +0200

    Testing the kernel remapping.

--- src/main.rs
+++ src/main.rs
@@ -24,17 +24,17 @@ use notOS::{println, print,
     kernel_components::{
         memory::{
             InfoPointer, BootInfoHeader, 
-            AreaFrameAllocator, ActivePageTable, Page,
-            EntryFlags,
-            frames::FrameAlloc
+            AreaFrameAllocator,
+            self,
         },
+        registers::control,
     },            
     Color,
 };
 
 #[no_mangle]
 pub extern "C" fn _start(_multiboot_information_address: usize) {
-    // All tests will be trapped in there instantly. This must be this way,
+    // All tests will be trapped here instantly. This must be this way,
     // because memory manipulations may cause undefined behavior for tests.
     #[cfg(test)]
     test_main();
@@ -49,8 +49,8 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
 
         let kernel_start = boot_info.kstart();
         let kernel_end = boot_info.kend();
-        let multiboot_start = _multiboot_information_address;
-        let multiboot_end = multiboot_start + ( boot_info.total() as usize );
+        let multiboot_start = boot_info.mstart();
+        let multiboot_end = boot_info.mend();
 
         let mut frame_allocator = AreaFrameAllocator::new(
             kernel_start as usize, 
@@ -60,23 +60,12 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
             memory_map_tag.memory_map_iter(),
         );
 
-        let mut page_table = unsafe { ActivePageTable::new() };
-        
-        let addr = 42 * 512 * 512 * 4096; // 42th P3 entry.
-        let page = Page::containing_address(addr);
-        let frame = frame_allocator.alloc().expect("No more frames to allocate.");
+        control::Cr0::enable_write_protect_bit();
 
-        println!(Color::CYAN; "None = {:?}, map to {:?}", page_table.translate(addr), frame);
-        page_table.map_to(page, frame, EntryFlags::empty(), &mut frame_allocator);
-        println!(Color::LIGHTBLUE; "Some = {:?}", page_table.translate(addr));
-        println!(Color::LIGHTGREEN; "Next free frame: {:?}", frame_allocator.alloc());
-        
-        println!("{:#x}", unsafe {
-            *(Page::containing_address(addr).start_address() as *const u64)
-        });
-
-        page_table.unmap(Page::containing_address(addr), &mut frame_allocator);
-        println!(Color::MAGENTA; "None = {:?}", page_table.translate(addr));
+        println!("Time to remap!");
+        // remaping the kernel
+        memory::remap_kernel(&mut frame_allocator, &boot_info);
+        println!("Kernel sections are now remapped!");
     }   
 
     main();
@@ -85,6 +74,6 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
 #[allow(dead_code, unreachable_code)]
 fn main() -> ! {
     println!(Color::BLUE; "Hello memory!");
-    
+
     loop {}
 }
