311d48a075c70788fd035e11b6ede7795f7ac9b0
Notforest <notforest69@gmail.com>
2023-09-14 03:34:34 +0200

    Checking out the new Bump allocator.

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,7 @@ static HEADER_START_FUNC: unsafe extern "C" fn() = header_start;
 static HEADER_END_FUNC: unsafe extern "C" fn() = header_end;
 
 /// This is the main binary (kernel) space. As the library will build in, more new features will be added further.
-use notOS::{println, print, 
-    kernel_components::{
-        memory::{
-            InfoPointer, BootInfoHeader, 
-            AreaFrameAllocator,
-            self,
-        },
-        registers::control,
-    },            
-    Color,
-};
+use notOS::{println, print, kernel_components::{memory::{self, memory_module::{InfoPointer, BootInfoHeader}}, registers::control}, Color};
 
 #[no_mangle]
 pub extern "C" fn _start(_multiboot_information_address: usize) {
@@ -42,30 +32,10 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
     // This part will only be compiled during debugging.
     #[cfg(debug_assertions)] {
         let boot_info = unsafe { InfoPointer::load(_multiboot_information_address as *const BootInfoHeader ) }.unwrap();
-        let memory_map_tag = boot_info.memory_map_tag()
-            .expect("Memory map tag required.");
-        let elf_sections_tag = boot_info.elf_sections_tag()
-            .expect("Elf-sections tag required.");
-
-        let kernel_start = boot_info.kstart();
-        let kernel_end = boot_info.kend();
-        let multiboot_start = boot_info.mstart();
-        let multiboot_end = boot_info.mend();
-
-        let mut frame_allocator = AreaFrameAllocator::new(
-            kernel_start as usize, 
-            kernel_end as usize, 
-            multiboot_start, 
-            multiboot_end,
-            memory_map_tag.memory_map_iter(),
-        );
-
+        
         control::Cr0::enable_write_protect_bit();
 
-        println!("Time to remap!");
-        // remaping the kernel
-        memory::remap_kernel(&mut frame_allocator, &boot_info);
-        println!("Kernel sections are now remapped!");
+        memory::init(&boot_info);
     }   
 
     main();
@@ -73,7 +43,24 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
 
 #[allow(dead_code, unreachable_code)]
 fn main() -> ! {
-    println!(Color::BLUE; "Hello memory!");
+    use notOS::Vec;
+    
+    println!(Color::BLUE; "Hello stack and heap memory!");
+    println!("Lets allocate something.");
+
+    let mut vector: Vec<usize> = Vec::new();
+
+    for i in 0..8000 {
+        vector.push(i);
+    }
+
+    println!("The vector has {} elements.", vector.len);
+
+    println!(Color::DARKGRAY; "Time to overflow the heap!.");
+
+    for i in 0.. {
+        vector.push(i);
+    }
 
     loop {}
 }
