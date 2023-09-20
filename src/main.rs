b84485b734d8b7caefb8fe0f803aaea948d12c57
Notforest <notforest69@gmail.com>
2023-09-20 21:44:11 +0200

    Testing the node allocator.

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ static HEADER_START_FUNC: unsafe extern "C" fn() = header_start;
 static HEADER_END_FUNC: unsafe extern "C" fn() = header_end;
 
 /// This is the main binary (kernel) space. As the library will build in, more new features will be added further.
-use notOS::{print, warn, kernel_components::{memory::{self, memory_module::{InfoPointer, BootInfoHeader}}, registers::control}, Color};
+use notOS::{warn, kernel_components::{memory::{self, memory_module::{InfoPointer, BootInfoHeader}, allocators::SubAllocator}, registers::control}, Color, GLOBAL_ALLOCATOR, NODE_ALLOC};
 
 #[no_mangle]
 pub extern "C" fn _start(_multiboot_information_address: usize) {
@@ -33,8 +33,11 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
     {
         let boot_info = unsafe { InfoPointer::load(_multiboot_information_address as *const BootInfoHeader ) }.unwrap();
         
+        // The global allocator is a mutable static that do not use any locking 
+        // algorithm, so any operation on it, is unsafe.
+        unsafe { GLOBAL_ALLOCATOR.r#use(&NODE_ALLOC) };
+        
         control::Cr0::enable_write_protect_bit();
-
         memory::init(&boot_info);
     }
 
@@ -42,31 +45,19 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
     #[cfg(debug_assertions)] {
         warn!("DEBUG MODE ON!");
     }
-
+    
     main();
 }
 
 #[allow(dead_code, unreachable_code)]
 fn main() -> ! {
     use notOS::Vec;
-    
     {
-        let mut vector = Vec::new();
-        let text = "!god yzal eht revo spmuj xof nworb kciuq A";
-
-        for w in text.chars() {
-            vector.push(w);
+        let mut vector: Vec<u8> = Vec::new();
+    
+        for i in 0..30 {
+            vector.push(i);
         }
-
-        while vector.len() > 0 {
-            print!(Color::CYAN; "{}", vector.pop().unwrap());
-        }
-    }
-
-    let vector = Vec::from_array(&[1, 2, 4, 5, 6, 7, 10]);
-
-    for i in vector {
-        print!(" {}", i);
     }
 
     loop {}
