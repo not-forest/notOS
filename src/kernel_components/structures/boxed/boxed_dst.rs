6d57f0aa1cbcbb8536c4c10c561b1ee411216e82
Notforest <notforest69@gmail.com>
2023-09-21 18:52:13 +0200

    Added the normal new() method, for allocating data types on the heap like in regular Box.

--- src/kernel_components/structures/boxed/boxed_dst.rs
+++ src/kernel_components/structures/boxed/boxed_dst.rs
@@ -1,30 +1,37 @@
-// This type behaves similar to the regular
-// `Box` type except that it ensure the same layout is used for the (explicit)
-// allocation and the (implicit) deallocation of memory.
+/// Implementation of the boxed data structure that save memory on the heap.
+/// 
+/// This type behaves similar to the regular `Box` type except that it ensure the 
+/// same layout is used for the (explicit) allocation and the (implicit) deallocation of memory.
 
 use crate::Bytes;
 use crate::kernel_components::memory::{
     tags::{Tag, TagTrait, TagTypeId},
-    allocators::global_alloc::{GAllocator, GLOBAL_ALLOCATOR},
+    allocators::global_alloc::GLOBAL_ALLOCATOR,
 };
 use core::alloc::{GlobalAlloc, Layout};
 use core::marker::PhantomData;
-use core::mem::size_of;
 use core::ops::Deref;
+use core::mem::{self, MaybeUninit};
 use core::ptr::{self, NonNull};
 
+/// Box implementation for the OS.
+/// 
+/// Provides the heap allocations for DST that lies within the box. Boxes provide 
+/// ownership for this allocation, and drop their contents when they go out of scope. 
+/// Boxes ensure that they never allocate more than `isize::MAX` bytes.
 #[derive(Debug, Eq)]
 pub struct BoxedDst<T: ?Sized> {
-    ptr: core::ptr::NonNull<T>,
+    ptr: NonNull<T>,
     layout: Layout,
     _marker: PhantomData<T>,
 }
 
 impl<T: TagTrait<Metadata = usize> + ?Sized> BoxedDst<T> {
-    pub fn new(content: &[u8]) -> Self {
+    /// Creates a new instance of 'BoxedDst', that holds 
+    pub fn new_tag_dst(content: &[u8]) -> Self {
         const ALIGN: usize = 4;
 
-        let tag_size = size_of::<TagTypeId>() + size_of::<u32>() + content.len();
+        let tag_size = mem::size_of::<TagTypeId>() + mem::size_of::<u32>() + content.len();
         let alloc_size = (tag_size + 7) & !7;
         let layout = Layout::from_size_align(alloc_size, ALIGN).unwrap();
         let ptr = unsafe { GLOBAL_ALLOCATOR.alloc(layout) };
@@ -52,6 +59,36 @@ impl<T: TagTrait<Metadata = usize> + ?Sized> BoxedDst<T> {
 
         Self {
             ptr: NonNull::new(raw).unwrap(),
+            layout,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> BoxedDst<T> {
+    /// Creates a new instance of 'BoxedDst<T>'
+    /// 
+    /// Allocates memory on the heap, and places 'T' into it.
+    /// 
+    /// # Panics
+    /// 
+    /// If the provided data type, is a ZST, it does not allocate any memory and will
+    /// panic in the assertion.
+    pub fn new(content: T) -> Self {
+        let content_size = mem::size_of::<T>();
+
+        assert!(content_size != 0, "The size of provided data type must not be zero.");
+
+        let content_align = mem::align_of::<T>();
+        let layout = Layout::from_size_align(content_size, content_align).unwrap();
+        let ptr = unsafe { GLOBAL_ALLOCATOR.alloc(layout) } as *mut T;
+        
+        unsafe {
+            ptr.write(content)
+        }
+
+        Self {
+            ptr: NonNull::new(ptr).unwrap(),
             layout,
             _marker: PhantomData,
         }
