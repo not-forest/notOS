e8ea3cde13b18d4d025b5c6bc2def41e9bb98ad4
Notforest <notforest69@gmail.com>
2023-09-26 22:14:48 +0200

    Testing the concurrent list. (In a one thread invironment... Yes).

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ static HEADER_START_FUNC: unsafe extern "C" fn() = header_start;
 static HEADER_END_FUNC: unsafe extern "C" fn() = header_end;
 
 /// This is the main binary (kernel) space. As the library will build in, more new features will be added further.
-use notOS::{warn, kernel_components::{memory::{self, memory_module::{InfoPointer, BootInfoHeader}, allocators::SubAllocator}, registers::control}, Color, GLOBAL_ALLOCATOR, BUMP_ALLOC};
+use notOS::{warn, kernel_components::{memory::{self, memory_module::{InfoPointer, BootInfoHeader}}, registers::control}, Color, GLOBAL_ALLOCATOR, BUMP_ALLOC};
 
 #[no_mangle]
 pub extern "C" fn _start(_multiboot_information_address: usize) {
@@ -51,15 +51,32 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
 
 #[allow(dead_code, unreachable_code)]
 fn main() -> ! {
-    use notOS::BoxedDst;
-
-    #[derive(Debug)]
-    struct K {}
+    use notOS::kernel_components::structures::thread_safe::ConcurrentList;
+    use notOS::{println, print};
 
     {
-        let boxed = BoxedDst::new(K {});
+        let mut list = ConcurrentList::new();
 
-        notOS::println!("{:?}", *boxed);
+        for i in (0..10).rev() {
+            list.push(i);
+        }
+
+        list.remove(4);
+        list.remove(7);
+
+        for node in list.iter() {
+            print!("{}, ", node);
+        }
+        
+        println!("{}", list.len());
+
+        for i in 0..list.len() {
+            list.modify(0, i);
+        }
+
+        for node in list.iter() {
+            print!("{}, ", node);
+        }
     }
 
     loop {}
