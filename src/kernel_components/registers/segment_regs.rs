9e4d7ee1a7587dd7f11e5d4122c1e4c618090b12
Notforest <notforest69@gmail.com>
2023-10-23 21:36:25 +0200

    Implemented segment for all segments. Implemented segment64 for fs and gs. Changed the traits structure and made them unsafe.

--- src/kernel_components/registers/segment_regs.rs
+++ src/kernel_components/registers/segment_regs.rs
@@ -12,10 +12,11 @@
 
 use super::{
     control::Cr4Flags,
-    ms::Msr,
+    ms::{Msr, FSBase, GSBase},
 };
 use crate::kernel_components::arch_x86_64::PrivilegeLevel;
 use crate::VirtualAddress;
+use core::arch::asm;
 
 /// Trait that represent the x86 segment.
 /// 
@@ -23,7 +24,7 @@ use crate::VirtualAddress;
 /// 
 /// The segment registers in pure real-mode are limited to 16 bits for addressing and represented
 /// as segment selectors. In 64-bit mode, most of the segmentation functionality is disabled.
-pub trait Segment {
+pub unsafe trait Segment {
     /// Reads data from the provided segment.
     fn read() -> SegmentSelector;
     /// Writes data to the provided segment.
@@ -39,9 +40,9 @@ pub trait Segment {
 /// 
 /// This type of segment can be used in 64-bit mode, even though not fully. This trait is only
 /// valuable for FS and GS segments, which have a base that is 64 bit.
-pub trait Segment64Bit<T>: Segment where T: Msr {
+pub unsafe trait Segment64Bit<T>: Segment where T: Msr {
     /// A msr base which can be either fsbase, gsbase or kernelgsbase.
-    const BASE: T;
+    const BASE: u32;
 
     /// Reads the base address of the segment
     fn read_base() -> VirtualAddress;
@@ -65,7 +66,7 @@ pub trait Segment64Bit<T>: Segment where T: Msr {
 /// if it does correspond then the CPL level of the selector used is changed accordingly.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
-pub struct SegmentSelector(u16);
+pub struct SegmentSelector(pub u16);
 
 impl SegmentSelector {
     /// Creates a new instance of 'SegmentSelector'.
@@ -95,36 +96,137 @@ impl SegmentSelector {
     pub const fn get_index(&self) -> u16 {
         self.0 >> 3
     }
-
+    
+    /// Returns the privilege level of the segment.
     #[inline]
     pub const fn get_privilege_level(&self) -> u16 {
         self.0 & 0x6
     }
 }
 
+#[doc(hidden)]
+macro_rules! implement_segment {
+    ($type:ty, $name:literal) => {
+        unsafe impl Segment for $type {
+            #[inline]
+            fn read() -> SegmentSelector {
+                let segment: u16;
+
+                unsafe {
+                    asm!(
+                        concat!("mov {0:x}, ", $name),
+                        out(reg) segment, 
+                        options(nomem, nostack, preserves_flags)
+                    );
+                }
+
+                SegmentSelector(segment)
+            }
+
+            #[inline]
+            unsafe fn write(selector: SegmentSelector) {
+                unsafe {
+                    asm!(
+                        concat!("mov ", $name, ", {0:x}"), 
+                        in(reg) selector.0, 
+                        options(nostack, preserves_flags)
+                    );
+                }
+            }
+        }
+        
+    };
+}
+
+#[doc(hidden)]
+macro_rules! implement_segment_u64 {
+    ($type:ty, $name:literal, $base:ty) => {
+        implement_segment!($type, $name);
+
+        unsafe impl<T: Msr> Segment64Bit<T> for $type {
+            const BASE: u32 = <$base>::MSR;
+
+            #[inline]
+            fn read_base() -> VirtualAddress {
+                let addr: VirtualAddress;
+
+                unsafe {
+                    asm!(concat!("rd", $name, "base {}"), 
+                    out(reg) addr, 
+                    options(nomem, nostack, preserves_flags));
+                }
+
+                addr
+            }
+
+            #[inline]
+            unsafe fn write_base(base: VirtualAddress) {
+                unsafe{
+                    asm!(concat!("wr", $name, "base {}"), 
+                    in(reg) base, 
+                    options(nostack, preserves_flags));
+                }
+            }
+        }
+    };
+}
 
 /// Code segment (CS) is used for addressing memory location in the code segment of the 
 /// memory, where the executable program is stored.
 #[derive(Debug)]
 pub struct CodeSegment;
 
+unsafe impl Segment for CodeSegment {
+    #[inline]
+    fn read() -> SegmentSelector {
+        let segment: u16;
+        
+        unsafe {
+            asm!(
+                "mov {0:x}, cs",
+                out(reg) segment, 
+                options(nomem, nostack, preserves_flags)
+            );
+        }
+
+        SegmentSelector(segment)
+    }
+
+    #[inline]
+    unsafe fn write(selector: SegmentSelector) {
+        unsafe {
+            asm!(
+                "push {selector}",
+                "lea {tmp}, [1f + rip]",
+                "push {tmp}",
+                "retfq",
+                "1:",
+                selector = in(reg) u64::from(selector.0),
+                tmp = lateout(reg) _,
+                options(preserves_flags),
+            );
+        }
+    }
+}
+
 /// Stack segment (SS) is used for addressing stack segment of the memory. The stack 
 /// segment is that segment of memory which is used to store stack data.
 #[derive(Debug)]
-pub struct StackSegment;
+pub struct StackSegment; implement_segment!(StackSegment, "ss");
 
 /// Data segment (DS) points to the data segment of the memory where the data is stored.
 #[derive(Debug)]
-pub struct DataSegment;
+pub struct DataSegment; implement_segment!(DataSegment, "ds");
 
 /// Extra segment (ES) also refers to a segment in the memory which is another data segment in the memory.
 #[derive(Debug)]
-pub struct ExtraSegment;
+pub struct ExtraSegment; implement_segment!(ExtraSegment, "es");
 
 /// Function segment (FS) is undefined at a hardware level and can be used how the OS decides.
 #[derive(Debug)]
-pub struct FunctionSegment;
+pub struct FunctionSegment; implement_segment_u64!(FunctionSegment, "fs", FSBase);
 
 /// General segment (GS) is undefined at a hardware level and can be used how the OS decides.
 #[derive(Debug)]
-pub struct GeneralSegment;
+pub struct GeneralSegment; implement_segment_u64!(GeneralSegment, "gs", GSBase);
+
