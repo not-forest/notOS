ca249b3f3a0986d912cd626e7b47054fb8a14de2
Notforest <notforest69@gmail.com>
2023-10-27 10:07:57 +0200

    Testing the GDT. When writing new values in cs segment, we are doing a far jump, which does involve GDT. So it must be well cofigured to work properly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 #![allow(non_snake_case)]
-#![feature(custom_test_frameworks, used_with_arg)]
+#![feature(custom_test_frameworks, used_with_arg, abi_x86_interrupt)]
 #![test_runner(notOS::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -20,7 +20,13 @@ static HEADER_START_FUNC: unsafe extern "C" fn() = header_start;
 static HEADER_END_FUNC: unsafe extern "C" fn() = header_end;
 
 /// This is the main binary (kernel) space. As the library will build in, more new features will be added further.
-use notOS::{println, warn, kernel_components::{memory::{self, memory_module::{InfoPointer, BootInfoHeader}}, registers::control}, Color, GLOBAL_ALLOCATOR, BUMP_ALLOC};
+use notOS::{
+    println, warn, 
+    kernel_components::{
+        memory::{self, memory_module::{InfoPointer, BootInfoHeader}}, 
+        registers::{control, ms}}, 
+        Color, GLOBAL_ALLOCATOR, BUMP_ALLOC
+    };
 
 #[no_mangle]
 pub extern "C" fn _start(_multiboot_information_address: usize) {
@@ -34,6 +40,7 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
         let boot_info = unsafe { InfoPointer::load(_multiboot_information_address as *const BootInfoHeader ) }.unwrap();
         
         control::Cr0::enable_write_protect_bit();
+        ms::EFER::enable_nxe_bit();
 
         // The global allocator is a mutable static that do not use any locking 
         // algorithm, so any operation on it, is unsafe.
@@ -52,20 +59,34 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
 
 #[allow(dead_code, unreachable_code)]
 fn main() -> ! {
-    use notOS::kernel_components::arch_x86_64::{GDT, segmentation::TSS};
+    use notOS::kernel_components::arch_x86_64::PrivilegeLevel;
+
+    use notOS::kernel_components::registers::segment_regs::{
+        Segment, SegmentSelector, StackSegment, CodeSegment
+    };
+
+    use notOS::kernel_components::arch_x86_64::segmentation::{
+        TSS, GDT, GLOBAL_DESCRIPTOR_TABLE
+    };
     
     static TASK_STATE_SEGMENT: TSS = TSS::new();
 
-    use notOS::single;
-    single! {
-        pub GLOBAL_DESC_TABLE: GDT = GDT::flat_setup(&TASK_STATE_SEGMENT);
+    unsafe {
+        GLOBAL_DESCRIPTOR_TABLE.reinit(GDT::flat_setup(&TASK_STATE_SEGMENT));
+        GLOBAL_DESCRIPTOR_TABLE.load_table();
+        println!("{:#x}", GLOBAL_DESCRIPTOR_TABLE.addr());
+        println!("{:#?}", GLOBAL_DESCRIPTOR_TABLE.as_dt_ptr());
+
+        CodeSegment::write(
+            SegmentSelector::new(1, false, PrivilegeLevel::KernelLevel)
+        );
+
+        StackSegment::write(
+            SegmentSelector::new(2, false, PrivilegeLevel::KernelLevel)
+        );
     }
 
-    GLOBAL_DESC_TABLE.load_table();
-
-    println!("{:?}\n", GLOBAL_DESC_TABLE);
-
-    println!(Color::CYAN; "Hello descriptors, hello segments, hello GDT!!!");
+    println!(Color::LIGHTCYAN; "Hello interrupts.");
 
     loop {}
 }
