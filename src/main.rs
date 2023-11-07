715726edff2065d34b76d1add6bec8924036ae9e
Notforest <notforest69@gmail.com>
2023-11-07 18:27:05 +0100

    Testing out the keyboard interrupt.

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,10 @@ static HEADER_END_FUNC: unsafe extern "C" fn() = header_end;
 use notOS::{
     println, warn, single,
     kernel_components::{
+        arch_x86_64::interrupts,
         memory::MMU, 
         registers::{control, ms}}, 
-        Color, GLOBAL_ALLOCATOR, BUMP_ALLOC,
+        GLOBAL_ALLOCATOR, BUMP_ALLOC,
     };
 
 #[no_mangle]
@@ -52,9 +53,12 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
 
     use notOS::kernel_components::arch_x86_64::interrupts::{
         handler_functions::predefined::*,
+        handler_functions::software::*,
         INTERRUPT_DESCRIPTOR_TABLE,
         GateDescriptor,
     };
+
+    use notOS::kernel_components::arch_x86_64::controllers::PROGRAMMABLE_INTERRUPT_CONTROLLER;
 
     // Memory initialization.
     // The global allocator is a mutable static that do not use any locking 
@@ -64,6 +68,7 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
     // New MMU structure makes it easier to handle memory related commands.
     let mut MEMORY_MANAGEMENT_UNIT = MMU::new_init(_multiboot_information_address);
 
+    // Enabling the nxe bit and write protect bit.
     control::Cr0::enable_write_protect_bit();
     ms::EFER::enable_nxe_bit();
 
@@ -72,33 +77,54 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
     }
 
     unsafe {
+        // Setting up the stack for IST.
         MEMORY_MANAGEMENT_UNIT.set_interrupt_stack(&mut TASK_STATE_SEGMENT,0,1);
 
+        // Rewrite the static GDT. It will use the flat setup.
         GLOBAL_DESCRIPTOR_TABLE.reinit(GDT::flat_setup(&TASK_STATE_SEGMENT));
-        GLOBAL_DESCRIPTOR_TABLE.load_table();
+        GLOBAL_DESCRIPTOR_TABLE.load_table(); // Loads the table to the CPU.
 
+        // Reloading the CS segment.
         CodeSegment::write(
             SegmentSelector::new(1, false, PrivilegeLevel::KernelLevel)
         );
 
+        // Reloading the SS segment.
         StackSegment::write(
             SegmentSelector::new(2, false, PrivilegeLevel::KernelLevel)
         );
 
+        // Reloading the TSS segment.
         TSS::write(
             SegmentSelector::new(5, false, PrivilegeLevel::KernelLevel)
         );
 
+        // Exception gates.
         let gate_div = GateDescriptor::new_trap(DIVISION_BY_ZERO);
         let gate_break = GateDescriptor::new_trap(BREAKPOINT);
         let gate_double_fault = GateDescriptor::new_trap(DOUBLE_FAULT);
         let gate_page_fault = GateDescriptor::new_trap(PAGE_FAULT);
 
+        // Interrupt gates.
+        let gate_timer = GateDescriptor::new_interrupt(TIMER_INTERRUPT);
+        let gate_keyboard = GateDescriptor::new_interrupt(KEYBOARD_INTERRUPT);
+
+        // Pushing the gates into the IDT.
         INTERRUPT_DESCRIPTOR_TABLE.push(0, gate_div);
         INTERRUPT_DESCRIPTOR_TABLE.push(3, gate_break);
         INTERRUPT_DESCRIPTOR_TABLE.push(8, gate_double_fault);
         INTERRUPT_DESCRIPTOR_TABLE.push(14, gate_page_fault);
+
+        INTERRUPT_DESCRIPTOR_TABLE.push(32, gate_timer);
+        INTERRUPT_DESCRIPTOR_TABLE.push(33, gate_keyboard);
+
+        // Loading the IDT table to the CPU.
         INTERRUPT_DESCRIPTOR_TABLE.load_table();
+
+        // Remapping the PIC controller.
+        PROGRAMMABLE_INTERRUPT_CONTROLLER.lock().reinit_chained(32).remap();
+
+        interrupts::enable();
     }
     
     main();
@@ -106,6 +132,8 @@ pub extern "C" fn _start(_multiboot_information_address: usize) {
 
 #[allow(dead_code, unreachable_code)]
 fn main() -> ! {
+
+    println!("Hello keyboard");
 
     loop {}
 }
