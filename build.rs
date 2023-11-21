979ddde31fb7ae0d96103a6e43bd9e7fda8e7f3f
Notforest <notforest69@gmail.com>
2023-11-21 12:29:41 +0100

    Made the build script compatible to different environments.

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-search=native=/home/notforest/Documents/notOS/build");
-    println!("cargo:rustc-link-arg=-T/home/notforest/Documents/notOS/src/arch/x86_64/linker.ld");
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    println!("cargo:rustc-link-search=native={}/build", current_dir.as_path().to_string_lossy());
+    println!("cargo:rustc-link-arg=-T{}/src/arch/x86_64/linker.ld", current_dir.as_path().to_string_lossy());
 }
