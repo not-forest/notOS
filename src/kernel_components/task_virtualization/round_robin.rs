6f647ce8bfc567803eabcbfe8cb59d6a45b4a158
Notforest <notforest69@gmail.com>
2023-12-20 13:13:37 +0100

    A round robin implementation for the task switch scheduler.

---
+++ src/kernel_components/task_virtualization/round_robin.rs
@@ -0,0 +1,82 @@
+//! A Round-Robin scheduler implementation module.
+
+use super::{Scheduler, Task, Thread, ThreadFn, Process};
+use crate::{GLOBAL_ALLOCATOR, single};
+use crate::kernel_components::structures::thread_safe::ConcurrentList;
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+single! {
+    pub mut ROUND_ROBIN: RoundRobin = RoundRobin::new();
+}
+
+/// A structure that represents the Round-Robin scheduler.
+/// 
+/// The scheduler returns the next task within the list.
+pub struct RoundRobin {
+    /// Index of the current running task
+    current_task: AtomicUsize,
+    /// The list of all tasks
+    list: ConcurrentList<Task>,
+}
+
+impl RoundRobin {
+    /// Creates a new instance of the scheduler.
+    pub fn new() -> Self {
+        Self { 
+            list: ConcurrentList::new(unsafe { &mut GLOBAL_ALLOCATOR }),
+            current_task: AtomicUsize::new(0), 
+        }
+    }
+
+    /// Appends the task based on the provided thread
+    pub fn append_thread(&mut self, thread: &Thread) {
+        self.list.push(
+            Task {
+                pid: thread.pid,
+                tid: thread.tid,
+            }
+        );
+    }
+
+    /// Appends all the tasks of the given process.
+    pub fn append_process(&mut self, proc: &Process) {
+        for thread in proc.threads.iter() {
+            self.append_thread(thread)
+        }
+    }
+}
+
+impl Scheduler for RoundRobin {
+    fn append(&mut self, task: Task) {
+        self.list.push(task);
+    }
+
+    fn delete(&mut self, task: Task) {
+        if let Some(index) = self.list.index_of(task) {
+            self.list.remove(index);
+        }
+    }
+
+    fn schedule(&mut self) -> Option<&Task> {
+        let task = self.list.get(self.current_task.load(Ordering::Relaxed));
+        
+        let index = self.current_task.fetch_add(1, Ordering::SeqCst);
+
+        if index + 1 >= self.list.len() {
+            self.current_task.compare_exchange(
+                index + 1, 
+                0, 
+                Ordering::SeqCst, 
+                Ordering::Relaxed,
+            );
+        }
+        
+        task
+    }
+
+    unsafe fn clear(&mut self) {
+        for _ in 0..self.list.len() {
+            self.list.pop_front()
+        }
+    }
+}
