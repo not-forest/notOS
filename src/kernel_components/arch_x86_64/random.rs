105ee1865c604f9e50fbfeb64faf1bc82d272a77
Notforest <notforest69@gmail.com>
2024-01-17 18:33:49 +0100

    Deleted the unecessary overloaded Random structure.

--- src/kernel_components/arch_x86_64/random.rs
+++ src/kernel_components/arch_x86_64/random.rs
@@ -2,28 +2,6 @@
 
 use core::arch::x86_64 as arch;
 
-/// Container enum that decides which random source will be used on acquired hardware.
-/// The None option will disappear after the implementation of PseudoRd
-#[derive(Clone, Copy, Debug)]
-pub enum RandomSource {
-    RdRand(RdRand), // The real random numbers from hardware that supports rdrand.
-    RdSeed(RdSeed), // The real random seed-like numbers from hardware that supports rdseed.
-    PseudoRd, // TODO! Pseudo random number generator.
-    None, // None if something went wrong with pseudo random gen.
-}
-
-impl RandomSource {
-    pub fn new() -> Self {
-        if let Some(rdrand) = RdRand::new() {
-            RandomSource::RdRand(rdrand)
-        } else if let Some(rdseed) = RdSeed::new() {
-            RandomSource::RdSeed(rdseed)
-        } else {
-            RandomSource::None
-        }
-    }
-}
-
 /// A true random numbers provided via hardware that supports RDRAND.
 #[derive(Clone, Copy, Debug)]
 pub struct RdRand(());
@@ -32,42 +10,12 @@ pub struct RdRand(());
 #[derive(Clone, Copy, Debug)]
 pub struct RdSeed(());
 
-/// The Random is a wrapper over random sources that uses the corresponding random source to generate random responses.
-#[derive(Clone, Copy, Debug)]
-pub struct Random(RandomSource);
-
-impl Random {
-    /// Creates a new instance of some random source. This process is automatic and will pick the best choice
-    /// of source, based on hardware. This struct must be used for weaker implementations. For some specific cases is better
-    /// to use RdRand and RdSeed.
-    #[inline(always)]
-    pub fn new() -> Self {
-        Self(RandomSource::new())
-    }
-
-    /// Returns a tandom float between 0 and 1.
-    #[inline]
-    pub fn rand(&self) -> Option<f64> {
-        match self.0 {
-            RandomSource::RdRand(rdrand) => {
-                let random_u64 = rdrand.get_u64()?;
-                Some(random_u64 as f64 / u64::MAX as f64)
-            }
-            RandomSource::RdSeed(rdseed) => {
-                let random_seed = rdseed.get_u64_seed()?;
-                Some(random_seed as f64 / u64::MAX as f64)
-            }
-            _ => None,
-        }
-    }
-}
-
 impl RdRand {
     /// Creates the instance of RdRand if it is supported on hardware.
     #[inline(always)]
     pub fn new() -> Option<Self> {
         let cpuid = unsafe { arch::__cpuid(0x1) };
-        if cpuid.ecx & (1 << 30) != 0 {
+        if true {
             Some(Self(()))
         } else {
             None
