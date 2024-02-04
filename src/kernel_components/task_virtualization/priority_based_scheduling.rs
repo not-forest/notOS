399b1bcb64005dba52f719605fc2524b107d8fb7
Notforest <notforest69@gmail.com>
2024-02-04 23:49:35 +0100

    Added the priority based scheduler. It works in the way that every high priority tasks must end first before the lower priority ones could obtain the processor. A more RTOS type of scheduler.

---
+++ src/kernel_components/task_virtualization/priority_based_scheduling.rs
@@ -0,0 +1,100 @@
+/// An implementation of priority based scheduling algorithm.
+///
+/// This scheduler will only serve high priority processes until they done.
+/// TODO! Make the scheduler to change priorities based on halting.
+
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+use crate::kernel_components::structures::thread_safe::ConcurrentList;
+use crate::kernel_components::sync::Mutex;
+use crate::{single, GLOBAL_ALLOCATOR};
+
+use super::{Scheduler, Task, Thread, ThreadFn, Process, PROCESS_MANAGEMENT_UNIT};
+
+single! {
+    pub mut PRIORITY_SCHEDULER: PriorityScheduler = PriorityScheduler::new();
+}
+
+/// A priority based scheduler.
+///
+/// It will always choose the highest priority tasks more, hovewer this will not
+/// affect individual threads. If a high priority process have only one thread
+/// to execute, it will not schedule until the process is done executing.
+///
+/// This is a more RTOS styled scheduling algorithm.
+pub struct PriorityScheduler {
+    /// Index of the current running task
+    current_task: AtomicUsize,
+    /// The list of all tasks
+    list: ConcurrentList<Task>,  
+}
+
+impl PriorityScheduler {
+    /// Creates a new instance of PriorityScheduler.
+    pub fn new() -> Self {
+        Self { 
+            list: ConcurrentList::new(unsafe { &mut GLOBAL_ALLOCATOR }),
+            current_task: AtomicUsize::new(0), 
+        }
+    }
+
+    /// Appends the task based on the provided thread
+    pub fn append_thread(&mut self, thread: &Thread) {
+        self.list.push(
+            Task {
+                pid: thread.pid,
+                tid: thread.tid,
+            }
+        );
+    }
+
+    /// Appends all the tasks of the given process.
+    pub fn append_process(&mut self, proc: &Process) {
+        for thread in proc.threads.iter() {
+            self.append_thread(thread)
+        }
+    }
+}
+
+impl Scheduler for PriorityScheduler {
+    fn append(&mut self, task: Task) {
+        self.list.push(task); 
+    }
+
+    fn delete(&mut self, task: Task) {
+        if let Some(index) = self.list.index_of(task) {
+            self.list.remove(index);
+        }
+    }
+
+    fn schedule(&mut self) -> Option<&Task> {
+        if let Some(mut current_task) = self.list.get(self.current_task.load(Ordering::Relaxed)) {
+            let mut pri = u8::MAX;
+
+            for task in self.list.iter() {
+                unsafe {
+                    if let Some(proc) = PROCESS_MANAGEMENT_UNIT.process_list.lock().get(task.pid) {
+                        if proc.priority < pri {
+                            current_task = task;
+                            pri = proc.priority;
+                        } else if proc.priority == 0 {
+                            current_task = task;
+                            break
+                        }
+                    }
+                }
+            }
+
+            return Some(current_task)
+        }
+        self.current_task.fetch_add(1, Ordering::SeqCst);
+
+        None
+    }
+
+    unsafe fn clear(&mut self) {
+        for _ in 0..self.list.len() {
+            self.list.pop_front()
+        }
+    }
+}
