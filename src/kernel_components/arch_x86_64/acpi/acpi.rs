71ce12d4929f7fa047aaeff7ea4e71e76f67f1df
Notforest <notforest69@gmail.com>
2024-04-05 21:55:04 +0200

    Splitted the RXSDT to two RSDT/XSDT tables. Now pointers are implemented in a separate module.

--- src/kernel_components/arch_x86_64/acpi/acpi.rs
+++ src/kernel_components/arch_x86_64/acpi/acpi.rs
@@ -1,12 +1,53 @@
+use alloc::borrow::ToOwned;
+use core::mem;
+
 /// Custom module for communication with ACPI.
 ///
 /// ACPI allows power management and provides different configurations for the OS
 /// to handle, like the amount of running threads for example. ACPI contains of
 /// different tables like RSDP, BGRT, FADT etc.
 
+use crate::kernel_components::os::UChar;
 pub use super::{
-    rsdt::RXSDT,
+    rsdt::{RSDT, XSDT},
 };
+
+/// Custom trait for all system description tables defined by ACPI specification.
+///
+///
+pub trait SystemDescriptionTable {
+    /// Signature defined by 'signature' field in ACPISDTHeader structure. Each SDT has it's
+    /// own signature and it is written in memory in table headers.
+    const SIGNATURE: &'static str;
+
+    /// Performs full SDT validation and will provide info about any error that occur.
+    ///
+    /// Validate SDT based on it's checksum provided in ACPI DST Header. Errors can be 
+    /// returned based on table type. All tables must be validated, even if they were obtained 
+    /// by the link of other tables.
+    fn validate(header: &ACPISDTHeader) -> Result<(), SDTValidationError> {
+        use SDTValidationError::*;
+
+        if !Self::checksum(header) {
+            return Err(CHECKSUM)
+        }
+
+        Ok(())
+    }
+
+    /// Makes checksum manually, Return true if everything is right.
+    fn checksum(header: &ACPISDTHeader) -> bool {
+        let slice: &[u8] = unsafe {
+            core::slice::from_raw_parts(
+                header as *const _ as *const u8,
+                mem::size_of::<ACPISDTHeader>(),
+            )
+        };
+
+        // All bytes in the structure must sum up to zero.
+        slice.iter().fold(0u8, |s, b| s.wrapping_add(*b)) == 0
+    }
+}
 
 /// Advanced Configuration and Power Interface.
 ///
@@ -25,16 +66,16 @@ pub struct ACPI;
 #[derive(Debug, Clone, Copy)]
 pub struct ACPISDTHeader {
     /// 4 byte or 8 byte signature field, which defines which table is being used.
-    signature: Signature,
+    pub signature: Signature,
     /// The size of the entire table.
     length: u32,
     /// The revision of the ACPI.
     revision: u8,
-    /// All bytes of the table summed butst be equal to 0. 
+    /// All bytes of the table summed must be equal to 0. 
     checksum: u8,
-    oem_id: OEMId,
-    oem_table_id: OEMTableId,
-    oem_revision: u32,
+    pub oem_id: [UChar; 6],
+    pub oem_table_id: [UChar; 8],
+    pub oem_revision: u32,
     creator_id: u32,
     creator_revision: u32,
 }
@@ -42,20 +83,29 @@ pub struct ACPISDTHeader {
 /// All ACPI tables have a 4 byte Signature field, except the RSDP which has
 /// an 8 byte one. This signature is used when the OS must determine which table
 /// it is useing at this moment.
+#[repr(C)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub(crate) enum Signature {
     /// Most ACPI tables use this.
-    Default([char; 4]),
-    /// Special only for RSDP table.
-    RSDP([char; 8]),
+    Default([UChar; 4]),
+    /// Special only for RSDT table.
+    RSDT([UChar; 8]),
 } 
 
-/// OEM-supplied string that identifies the OEM.
-#[repr(transparent)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub(crate) struct OEMId([char; 6]);
+impl PartialEq<str> for Signature {
+    fn eq(&self, other: &str) -> bool {
+        match self {
+            Self::Default(b) => b == other.as_bytes(),
+            Self::RSDT(b) => b == other.as_bytes(),
+        }
+    }
+}
 
-/// OEM-supplied string that identifies the OEM table ID.
-#[repr(transparent)]
+/// Custom defined errors, which may occur when validating tables.
+#[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub(crate) struct OEMTableId([char; 8]);
+pub enum SDTValidationError {
+    /// 8 bit checksum field is wrong. 
+    /// All bytes of the table summed must be equal to 0
+    CHECKSUM,
+}
---
+++ src/kernel_components/arch_x86_64/acpi/rsdp.rs
@@ -0,0 +1,178 @@
+//! Module that defines root system description pointers as both structure and tag.
+
+use super::acpi::{ACPISDTHeader, Signature, SystemDescriptionTable};
+use crate::kernel_components::memory::memory_module::MEMORY_MANAGEMENT_UNIT;
+use crate::kernel_components::memory::tags::{Tag, TagTrait, TagType, TagTypeId};
+use crate::kernel_components::os::UChar;
+
+use core::mem;
+
+/// Defines a set of errors that can occur while validating the RSDP or XSDP.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum RootPointerError {
+    /// Pointer had an invalid checksum.
+    CHECKSUM,
+    /// The signature is wrong.
+    SIGNATURE,
+    /// Unable to retrieve the Tag.
+    NOTAG,
+}
+
+/// Special pointer which points to the RSDT structure in ACPI version 1.0
+#[repr(C, packed)]
+#[derive(Debug, Clone)]
+pub(crate) struct RSDP {
+    signature: [UChar; 8],
+    /// Version 1.0 checksum
+    checksum: u8,
+    /// OEM-supplied string that identifies the OEM
+    oem_id: [UChar; 6],
+    /// SDTs revision.
+    revision: u8,
+    /// An actual pointer to the RSDT structure. Deprecated since version 2.0
+    pub ptr: u32,
+}
+
+impl RSDP {
+    /// Obtains the RSDP pointer from the MMU.
+    pub(crate) fn new() -> Result<Self, RootPointerError> {
+        if let Some(tag) = unsafe { MEMORY_MANAGEMENT_UNIT.get_rsdp() } { 
+            crate::println!("{:#?}, {:#?}, {:#x?}", tag.size, tag.tag_type, tag.rsdp);
+            
+            let rsdp = tag.rsdp.clone();
+
+            // Validating the rsdp right away.
+            match rsdp.validate() {
+                Ok(_) => Ok(rsdp),
+                Err(e) => Err(e),
+            }
+        } else {
+            Err(RootPointerError::NOTAG)
+        }
+    }
+
+    /// Returns true if checksum is valid.
+    pub fn checksum(&self) -> bool {
+        let slice: &[u8] = unsafe {
+            core::slice::from_raw_parts(
+                self as *const _ as *const u8,
+                mem::size_of::<Self>(),
+            )
+        };
+
+        // All bytes in the structure must sum up to zero.
+        slice.iter().fold(0u8, |s, b| s.wrapping_add(*b)) == 0
+    }
+
+    /// Validates the RSDP pointer.
+    pub fn validate(&self) -> Result<(), RootPointerError> {
+        // Signature check.
+        if self.signature != *b"RSD PTR " {
+            return Err(RootPointerError::SIGNATURE)
+        }
+
+        // Checksum check.
+        if !self.checksum() {
+            return Err(RootPointerError::CHECKSUM)
+        }
+
+        Ok(())
+    }
+}
+
+/// Special pointer which points to the XSDT structure in ACPI version 2.0
+#[repr(C, packed)]
+#[derive(Debug, Clone)]
+pub(crate) struct XSDP {
+    /// All fields in RSDP are also present in XSDT, hovewer the ptr field is deprecated.
+    signature: [UChar; 8],
+    /// Version 1.0 checksum
+    _checksum_legacy: u8,
+    /// OEM-supplied string that identifies the OEM
+    oem_id: [UChar; 6],
+    /// SDTs revision.
+    revision: u8,
+    /// Deprecated since version 2.0
+    _ptr_legacy: u32,
+
+    /// The size of the entire table since offset 0 to the end.
+    length: u32,
+    /// An actual pointer to the XSDT structure.
+    pub ptr: u64,
+    /// Extended checksum.
+    checksum: u8,
+    _reserved: [u8; 3],
+}
+
+impl XSDP {
+    /// Obtains the XSDP pointer from the MMU.
+    pub(crate) fn new() -> Option<Self> {
+        if let Some(tag) = unsafe { MEMORY_MANAGEMENT_UNIT.get_xsdp() } {
+            crate::println!("{:#?}, {:#?}, {:#x?}", tag.size, tag.tag_type, tag.xsdp);
+            return Some(tag.xsdp.clone())
+        }
+        None
+    }
+
+    /// Returns true if checksum is valid.
+    pub fn checksum(&self) -> bool {
+        let slice: &[u8] = unsafe {
+            core::slice::from_raw_parts(
+                self as *const _ as *const u8,
+                self.length as usize,
+            )
+        };
+
+        // All bytes in the structure must sum up to zero.
+        slice.iter().sum::<u8>() == 0
+    }
+
+    /// Validates the XSDP pointer.
+    pub fn validate(&self) -> Result<(), RootPointerError> {
+        // Signature check.
+        if self.signature != *b"RSD PTR " {
+            return Err(RootPointerError::SIGNATURE)
+        }
+
+        // Checksum check.
+        if !self.checksum() {
+            return Err(RootPointerError::CHECKSUM)
+        }
+
+        Ok(())
+    }
+}
+
+/// Tag which contains a copy of RSDP pointer for ACPI v1.0
+///
+/// This is the tag from multiboot2 structure. It must be used to obtain the RSDP
+/// pointer for legacy ACPI v1.0
+#[derive(Clone)]
+#[repr(C)]
+pub struct ACPITagOld {
+    pub tag_type: TagTypeId,
+    pub size: u32,
+    rsdp: RSDP,
+}
+
+impl TagTrait for ACPITagOld {
+    const ID: TagType = TagType::AcpiOld;
+    fn dst_size(tag: &Tag) {}
+}
+
+/// Tag which contains a copy of XSDP pointer for ACPI v2.0
+///
+/// This is the tag from multiboot2 structure. It must be used to obtain the XSDP
+/// pointer for legacy ACPI v2.0
+#[derive(Clone)]
+#[repr(C)]
+pub struct ACPITagNew {
+    pub tag_type: TagTypeId,
+    pub size: u32,
+    xsdp: XSDP,
+}
+
+impl TagTrait for ACPITagNew {
+    const ID: TagType = TagType::AcpiNew;
+    fn dst_size(tag: &Tag) {}
+}
