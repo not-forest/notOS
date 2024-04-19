194fc14e137ab9fd410766bc9048d95f3d0f2db1
Notforest <notforest69@gmail.com>
2024-04-19 23:56:05 +0200

    Added new global ACPI related definitions. Better documented comments available.

--- src/kernel_components/arch_x86_64/acpi/acpi.rs
+++ src/kernel_components/arch_x86_64/acpi/acpi.rs
@@ -1,13 +1,16 @@
-use alloc::borrow::ToOwned;
-use core::mem;
-
 /// Custom module for communication with ACPI.
 ///
 /// ACPI allows power management and provides different configurations for the OS
 /// to handle, like the amount of running threads for example. ACPI contains of
 /// different tables like RSDP, BGRT, FADT etc.
 
-use crate::kernel_components::os::UChar;
+use alloc::borrow::ToOwned;
+use core::mem;
+use crate::{
+    bitflags, 
+    kernel_components::os::UChar
+};
+
 pub use super::{
     rsdt::{RSDT, XSDT},
 };
@@ -85,6 +88,25 @@ pub struct ACPISDTHeader {
     creator_revision: u32,
 }
 
+/// Additional structure that represent an extended address to the position of 
+/// different registers in ACPI. It provides the platform with a robust means 
+/// to describe register locations. This structure is used to express register 
+/// addresses within tables defined by ACPI.
+pub(crate) struct GenericAddressStructure {
+    /// The address space where the data structure or register exists.
+    addr_space: AddressSpaceID,
+    /// The size in bits of the given register. When addressing a data structure,
+    /// this field must stay zero.
+    bit_width: u8,
+    /// The bit offset of the given register at the given address. When addressing 
+    /// a data structure, this field must stay zero.
+    bit_offset: u8,
+    /// Specifies access size. 
+    access_size: AccessSize,
+    /// 64-bit address of the data structure or register in the fiven address space.
+    addr: usize,
+}
+
 /// All ACPI tables have a 4 byte Signature field, except the RSDP which has
 /// an 8 byte one. This signature is used when the OS must determine which table
 /// it is useing at this moment.
@@ -114,4 +136,49 @@ pub enum SDTValidationError {
     CHECKSUM,
     /// The signature does not match the trait's signature and therefore wrong.
     SIGNATURE,
+}
+
+/// Specifies access sizze. Unless otherwise defined by the Address Space ID.
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AccessSize {
+    UNDEFINED, BYTE, WORD, DWORD, QWORD 
+}
+
+bitflags! {
+    /// The Generic Address Structure (GAS) 
+    ///
+    /// It provides the platform with a robust means to describe register locations. This structure 
+    /// is used to express register addresses within tables defined by ACPI .
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    pub struct AddressSpaceID: u8 {
+        /// The 64-bit physical memory address (relative to the processor) of the register. 32-bit 
+        /// platforms must have the high DWORD set to 0.
+        const SYSTEM_MEMORY_SPACE               = 0x00,
+        /// The 64-bit I/O address (relative to the processor) of the register. 32-bit platforms 
+        /// must have the high DWORD set to 0.
+        const SYSTEM_IO_SPACE                   = 0x01,
+        /// PCI Configuration space addresses must be confined to devices on PCI Segment Group 0, 
+        /// bus 0. This restriction exists to accommodate access to fixed hardware prior to PCI 
+        /// bus enumeration. The format of addresses are defined as follows:
+        /// - highest reserved word (must be 0)
+        /// - PCI device number on bus 0
+        /// - PCI function number
+        ///
+        /// Longest Word Offset in the configuration space header For example: Offset 23h of Function 
+        /// 2 on device 7 on bus 0 segment 0 would be represented as: 0x0000000700020023.
+        const PCI_CONFIGUIRATION_SPACE          = 0x02,
+        const EMBEDDED_CONTROLLER               = 0x03,
+        const SMBUS                             = 0x04,
+        const SYSTEM_CMOS                       = 0x05,
+        /// PciBarTarget is used to locate a MMIO register on a PCI device BAR space.
+        const PCI_BAR_TARGET                    = 0x06,
+        const IPMI                              = 0x07,
+        const GENERAL_PURPOSE_IO                = 0x08,
+        const GENERIC_SERIAL_BUS                = 0x09,
+        const PLATFORM_COMMUNICATION_CHANNEL    = 0x0a,
+        // Addresses 0x0b to 0x7e are reserved.
+        const FUNCTIONAL_FIXED_HARDWARE         = 0x7f,
+        // Addresses 0x80 t0 0xbf are reserved.
+    };
 }
