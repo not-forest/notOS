0e791a53fe804bf17b7946b116d763af808f5dc1
Notforest <notforest69@gmail.com>
2024-04-26 21:58:51 +0200

    Huge changes related to RSDT data representation. Allocating extra memory for data which is already written in <1MB is not necessary, therefore both RSDT/XSDT is now represented as a header and a static slice to all pointers.

--- src/kernel_components/arch_x86_64/acpi/rsdt.rs
+++ src/kernel_components/arch_x86_64/acpi/rsdt.rs
@@ -1,13 +1,13 @@
-use alloc::borrow::ToOwned;
 /// Custom module that implements RSDT table. It contains pointers to all system
 /// description tables.
 
+use alloc::borrow::ToOwned;
 use alloc::boxed::Box;
 use super::acpi::{ACPISDTHeader, SDTValidationError, Signature, SystemDescriptionTable};
 use super::rsdp::{RSDP, XSDP};
 use crate::critical_section;
 
-use core::ptr;
+use core::{mem, ptr};
 pub use super::rsdp::{ACPITagNew, ACPITagOld};
 
 /// Root/Extended Root System Description Table.
@@ -21,7 +21,7 @@ pub struct RSDT {
     /// RSDT has 8-byte signature header.
     header: ACPISDTHeader,
     /// Pointers for ACPI version 1.0
-    ptrs: Box<[*mut u32]>,
+    ptrs: &'static [*mut u32],
 }
 
 impl SystemDescriptionTable for RSDT {
@@ -38,7 +38,13 @@ impl RSDT {
             RSDP::new()
         }).expect("Unable to read from RSDP.");
 
-        unsafe { ptr::read_unaligned(rsdp.ptr as *mut RSDT) } 
+        RSDT::_ptrs_map(rsdp)
+    }
+
+    /// Works the same as new, but can be used to fast things up when you already do aquire
+    /// a proper RSDP.
+    pub(crate) unsafe fn from_rsdp(rsdp: RSDP) -> Self {
+        RSDT::_ptrs_map(rsdp)
     }
     
     /// Finds the requested SDT table. 
@@ -50,7 +56,7 @@ impl RSDT {
     /// Will return an error if table was found but its validation failed. Will return
     /// Ok(None) if table was not found for some reason. Will return Ok(&T), where T is
     /// expected to be another SDT. 
-    fn find<T>(&self) -> Result<Option<&mut T>, SDTValidationError> where 
+    pub fn find<T>(&self) -> Result<Option<&mut T>, SDTValidationError> where 
         T: SystemDescriptionTable
     {
         for ptr in self.ptrs.iter().map(|ptr| ptr.cast::<ACPISDTHeader>()) {
@@ -75,6 +81,27 @@ impl RSDT {
 
         Ok(None)
     }
+
+    /// Gets the amount of vectors this table has.
+    pub fn ptrs_amount(&self) -> usize {
+        _ptrs_amount(self.header)
+    }
+
+    fn _ptrs_map(rsdp: RSDP) -> Self {
+        unsafe { 
+            // Getting the amount of pointers for RSDT.
+            let header = ptr::read_unaligned(rsdp.ptr as *mut ACPISDTHeader); 
+            let ptrptr = rsdp.ptr as usize + mem::size_of::<ACPISDTHeader>();
+            let amount = _ptrs_amount(header);
+
+            RSDT {
+                header, 
+                ptrs: ptr::slice_from_raw_parts(
+                    ptrptr as *const *mut u32, amount
+                ).as_ref().unwrap()
+            }
+        } 
+    }
 }
 
 
@@ -89,7 +116,7 @@ pub struct XSDT {
     /// XSDT has 8-byte signature header.
     header: ACPISDTHeader,
     /// Pointers for ACPI version 2.0
-    ptrs: Box<[*mut usize]>,
+    ptrs: &'static [*mut usize],
 }
 
 impl SystemDescriptionTable for XSDT {
@@ -106,8 +133,14 @@ impl XSDT {
             XSDP::new()
         }).expect("Unable to read from XSDP.");
 
-        unsafe { ptr::read_unaligned(xsdp.ptr as *mut XSDT) }
-    } 
+        XSDT::_ptrs_map(xsdp)
+    }
+
+    /// Works the same as new, but can be used to fast things up when you already do aquire
+    /// a proper RSDP.
+    pub(crate) unsafe fn from_rsdp(xsdp: XSDP) -> Self {
+        XSDT::_ptrs_map(xsdp)
+    }
 
     /// Finds the requested SDT table. 
     ///
@@ -118,7 +151,7 @@ impl XSDT {
     /// Will return an error if table was found but its validation failed. Will return
     /// Ok(None) if table was not found for some reason. Will return Ok(&T), where T is
     /// expected to be another SDT. 
-    fn find<T>(&self) -> Result<Option<&mut T>, SDTValidationError> where 
+    pub fn find<T>(&self) -> Result<Option<&mut T>, SDTValidationError> where 
         T: SystemDescriptionTable
     {
         for ptr in self.ptrs.iter().map(|ptr| ptr.cast::<ACPISDTHeader>()) {
@@ -144,4 +177,28 @@ impl XSDT {
         Ok(None)
     }
 
+    /// Gets the amount of vectors this table has.
+    pub fn ptrs_amount(&self) -> usize {
+        _ptrs_amount(self.header)
+    }
+
+    fn _ptrs_map(xsdp: XSDP) -> Self {
+        unsafe { 
+            // Getting the amount of pointers for RSDT.
+            let header = ptr::read_unaligned(xsdp.ptr as *mut ACPISDTHeader); 
+            let ptrptr = xsdp.ptr as usize + mem::size_of::<ACPISDTHeader>();
+            let amount = _ptrs_amount(header);
+
+            XSDT {
+                header,
+                ptrs: ptr::slice_from_raw_parts(
+                    ptrptr as *const *mut usize, amount
+                ).as_ref().unwrap()
+            }
+        } 
+    }
+}
+
+fn _ptrs_amount(h: ACPISDTHeader) -> usize { 
+    (h.length as usize - mem::size_of::<ACPISDTHeader>()) / 4 
 }
