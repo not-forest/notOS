5f8046f4a042e140d73d045137bb83f416762ad8
Notforest <notforest69@gmail.com>
2024-08-19 09:54:11 +0200

    Added new port type for reading 'wide' values from 'thin' ports sequentially.

--- src/kernel_components/arch_x86_64/ports.rs
+++ src/kernel_components/arch_x86_64/ports.rs
@@ -93,5 +93,100 @@ impl<T: Port<T>> GenericPort<T> {
     }
 } 
 
+/// A convenient representation of a port, that should be used as a serial-in parallel-out buffer.
+///
+/// Convenient to use when some large amount of data must be passed via thin port serially. For
+/// example passing words or dwords via byte-sized port by performing several reads.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SipoPort<T, W> where T: Port<T> {
+    port: u16,
+    access: PortAccessType,
+    _phantom: PhantomData<(W, T)>
+}
 
+impl<'a, T: Port<T> + Clone> SipoPort<T, T> {
+    /// Creates a new instance of SIPO port. 
+    #[inline]
+    pub fn new(port: u16, access: PortAccessType) -> Self {
+        Self {
+            port, access,
+            _phantom: PhantomData,
+        }
+    }
 
+    /// Writes the slice of values into the port, if the port is WRITEONLY or READWRITE. The size
+    /// of slice defines how much data will be provided at time
+    /// 
+    /// # Panics
+    /// 
+    /// Panics if the port is READONLY.
+    #[inline]
+    pub fn write_many(&self, values: &'a [T]) {
+        assert!(self.access != PortAccessType::READONLY, "The port is READONLY.");
+
+        unsafe {
+            for val in values {
+                T::write(self.port, val.clone());
+            }
+        }
+    }
+
+    /// Reads the values from the port in form of slice, if the port is READONLY or READWRITE. The
+    /// size of slice buffer defines how much data should be read in a single chunk.
+    /// 
+    /// # Panics
+    /// 
+    /// Panics if the port is WRITEONLY.
+    #[inline]
+    pub fn read_many(&self, buffer: &'a mut [T]) {
+        assert!(self.access != PortAccessType::READONLY, "The port is READONLY.");
+
+        unsafe {
+            for buf in buffer {
+                *buf = T::read(self.port);
+            }
+        }
+    }
+}
+
+impl SipoPort<u8, u16> {
+    /// Creates a new instance of SIPO port for shifting words.
+    #[inline]
+    pub fn new(port: u16, access: PortAccessType) -> Self {
+        Self {
+            port, access,
+            _phantom: PhantomData,
+        }
+    }
+
+    /// Writes the word value into the port, if the port is WRITEONLY or READWRITE. Word is being
+    /// written in little endian format.
+    /// 
+    /// # Panics
+    /// 
+    /// Panics if the port is READONLY.
+    #[inline]
+    pub fn write(&self, value: u16) {
+        assert!(self.access != PortAccessType::READONLY, "The port is READONLY.");
+
+        unsafe {
+            u8::write(self.port, value as u8);
+            u8::write(self.port, (value >> 8) as u8);
+        }
+    }
+
+    /// Reads the word value from the port, if the port is READONLY or READWRITE. Obtained value is
+    /// represented in little endian.
+    /// 
+    /// # Panics
+    /// 
+    /// Panics if the port is WRITEONLY.
+    #[inline]
+    pub fn read(&self) -> u16 {
+        assert!(self.access != PortAccessType::WRITEONLY, "The port is WRITEONLY.");
+
+        unsafe {
+            u16::from_le_bytes([u8::read(self.port), u8::read(self.port)])
+        }
+    }
+}
