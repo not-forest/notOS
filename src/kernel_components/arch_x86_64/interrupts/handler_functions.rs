b46c72f208fcf420aff30783beff4b586e1e3013
Notforest <notforest69@gmail.com>
2024-10-04 23:37:53 +0200

    Added the prologue function macro. Moved the predefined content to separate modules.

--- src/kernel_components/arch_x86_64/interrupts/handler_functions.rs
+++ src/kernel_components/arch_x86_64/interrupts/handler_functions.rs
@@ -1,4 +1,4 @@
-/// Module for defining the handler functions and their types.
+/// Module for defining the exception/interrupt handler functions and their types.
 
 use crate::kernel_components::arch_x86_64::segmentation::{SegmentDescriptor, SegmentSelector};
 use crate::kernel_components::arch_x86_64::descriptor_table::DescriptorTableType;
@@ -7,8 +7,27 @@ use core::ops::{Deref, DerefMut};
 use core::arch::asm;
 use core::mem;
 
+use super::INTERRUPT_DESCRIPTOR_TABLE;
+
+
+/// Must be inserted in the start of any handler function that shall be flagged. Can be used in 
+/// both exceptions and interrupts. Allows to halt certain tasks until the interrupt is caused.
+///
+/// This does the following:
+/// - marks a corresponding isr bit within the IDT table.
+#[macro_export]
+macro_rules! handler_function_prologue {
+    ($isr:expr) => {
+        unsafe {
+            INTERRUPT_DESCRIPTOR_TABLE
+                .with_int($isr, |bit| *bit = true);
+        }
+    };
+}
+
 /// A marker trait for all handler functions.
 pub trait HandlerFn: Sized + 'static {
+    /// Gets the virtual address of the handler function. Used to load in IDT.
     fn get_virtual_addr(self) -> VirtualAddress;
 }
 
@@ -59,321 +78,6 @@ pub type DivergingHandlerFunctionWithErrCode = unsafe extern "x86-interrupt" fn(
     error_code: ErrorCode
 ) -> !;
 implement_handler_type!(DivergingHandlerFunctionWithErrCode);
-
-/// A collection of predefined functions that can be used within the gates.
-pub mod predefined {
-    use crate::{println, print, debug, Color, critical_section};
-    use super::*;
-
-    #[no_mangle]
-    unsafe extern "x86-interrupt" fn division_by_zero_handler(stack_frame: InterruptStackFrame) -> ! {
-        println!(Color::RED; "EXCEPTION: Division by zero.");
-        debug!("{:#?}", stack_frame);
-        loop {}
-    }
-
-    #[no_mangle]
-    unsafe extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
-        println!(Color::RED; "EXCEPTION: Breakpoint");
-        debug!("{:#?}", stack_frame);
-    }
-
-    #[no_mangle]
-    unsafe extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame) {
-        println!(Color::RED; "EXCEPTION: Double Fault");
-        debug!("{:#?}", stack_frame);
-        loop {}
-    }
-
-    #[no_mangle]
-    unsafe extern "x86-interrupt" fn page_fault_handler(
-        stack_frame: InterruptStackFrame,
-        error_code: ErrorCode,
-    ) {
-        critical_section!(|| {
-            println!(Color::RED; "EXCEPTION: Page Fault");
-            debug!("{:#?}", stack_frame);
-    
-            print!("Error code flags: ");
-            for error in PageFaultErrorCode::as_array() {
-                if error.is_in(error_code.0) {
-                    print!("{:?} ", error);
-                }
-            } println!();
-        });
-        loop {}
-    }
-
-    /// A regular division by zero handler. ('#DE')
-    /// 
-    /// This function provides the error info and a current stack table information.
-    pub const DIVISION_BY_ZERO: DivergingHandlerFunction = division_by_zero_handler;
-    /// Sets a breakpoint. ('#BP')
-    /// 
-    /// Will provide a current stack table information.
-    pub const BREAKPOINT: HandlerFunction = breakpoint_handler;
-
-    /// Double fault handler. ('#DF')
-    /// 
-    /// Double fault occur when the entry for some function is not set to the
-    /// corresponding interrupt vector or a second exception occurs inside the
-    /// handler function of the prior exception.
-    /// 
-    /// It only works for a certain combinations of exceptions:
-    /// - '#DE' -> '#TS', '#NP', '#SS', 'GP';
-    /// - '#TS' -> '#TS', '#NP', '#SS', 'GP';
-    /// - '#NP' -> '#TS', '#NP', '#SS', 'GP';
-    /// - '#SS' -> '#TS', '#NP', '#SS', 'GP';
-    /// - '#GP' -> '#TS', '#NP', '#SS', 'GP';
-    /// - '#PF' -> '#TS', '#NP', '#SS', 'GP', '#PF';
-    pub const DOUBLE_FAULT: HandlerFunction = double_fault_handler;
-
-    /// A page fault function handler.
-    /// 
-    /// There are many ways for the page fault to occur, therefore the error code
-    /// must be used accordingly as it does provide additional info about the reason
-    /// of the page fault invocation.
-    pub const PAGE_FAULT: HandlerFunctionWithErrCode = page_fault_handler;
-}
-
-/// A collection of predefined software interrupts, that must be used with PIC or APIC
-/// interrupt controller.
-/// 
-/// # Warn
-/// 
-/// For any of this function, the interrupt controller must be reprogrammed to the desired
-/// interrupt vector. Every handler function entry must be 
-pub mod software {
-    use alloc::boxed::Box;
-    use core::any::Any;
-
-    use crate::kernel_components::arch_x86_64::interrupts::interrupt;
-    use crate::kernel_components::drivers::DRIVER_MANAGER;
-    use crate::kernel_components::memory::EntryFlags;
-    use crate::kernel_components::task_virtualization::{Thread, Scheduler, PROCESS_MANAGEMENT_UNIT, PRIORITY_SCHEDULER, ROUND_ROBIN, ThreadState};
-    use crate::{critical_section, debug, print, println, warn, Color};
-    use crate::kernel_components::arch_x86_64::controllers::{
-        PROGRAMMABLE_INTERRUPT_CONTROLLER,
-        PS2,
-    };
-    use super::*;
-   
-    /// Software timer interrupt handler
-    ///
-    /// This handler calls the task switch function, which allows the PMU to perform the task
-    /// switching.
-    #[no_mangle]
-    unsafe extern "x86-interrupt" fn timer_interrupt_handler(mut stack_frame: InterruptStackFrame) {
-        use crate::kernel_components::task_virtualization::{
-            Scheduler, ROUND_ROBIN, PRIORITY_SCHEDULER, 
-
-            PROCESS_MANAGEMENT_UNIT, 
-
-            ThreadFn, Thread,
-            ThreadState, ProcState,
-        };
-        use core::sync::atomic::Ordering;
-
-        // This thread input must be changed when the function call must be done.
-        //
-        // The input is a mutable reference, therefore it will be putted within the 'rdi' register.
-        let mut thread_input = 0;
-
-        // Pushing new process if it exist.
-        PROCESS_MANAGEMENT_UNIT.dequeue();
-
-        // Perform a task switch.
-        //
-        // With disabled software interrupts, getting a next thread from the scheduler
-        // and changing both instruction and stack pointers to the corresponding pointers
-        // of that thread.
-        critical_section!(|| {
-            // Getting the lock.
-            let mut pmu = PROCESS_MANAGEMENT_UNIT.process_list.lock();
-
-            // Trying to obtain current tasks, if they exist.
-            if let Some(task) = ROUND_ROBIN.current() { 
-                if let Some(process) = pmu.get_mut(task.pid) {
-                    if let Some(thread) = process.find_thread_mut(task.tid) {
-                        // Doing some tasks to the previous thread, based on it's state.
-                        match thread.thread_state {
-                            ThreadState::RUNNING => {
-                                // Writting the old values to the thread.
-                                let new_stack = thread.stack_ptr.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |_| Some(stack_frame.stack_ptr));
-                                let new_ip = thread.instruction_ptr.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |_| Some(stack_frame.instruction_pointer));
-                            },
-                            _ => (),
-                        }
-                    }
-                }
-            }
-            // Trying to obtain some new tasks from a scheduler if some.
-            loop {
-                if let Some(task) = ROUND_ROBIN.schedule() {
-                    // Trying to find the process by task's pid.
-                    //
-                    // If not exists, we can easily delete all tasks with this pid.
-                    if let Some(process) = pmu.get_mut(task.pid) {
-                        // Trying to find the thread by task's tid.
-                        //
-                        // If not exists, we can delete this specific task.
-                        if let Some(thread) = process.find_thread_mut(task.tid) {
-                            // Doing different tasks based on the thread state.
-                            match thread.thread_state {
-                                ThreadState::INIT => {
-                                    // If the thread is only about to run we must provide some additional information for it.
-                                    //
-                                    // This basically means that we must perform the fast-call calling convention manually, so the
-                                    // thread can use a mutable reference to itself and perform the instructions.
-                                    thread_input = thread as *const _ as usize;
-                                    // Changing the state to running, which will not affect the thread's input.
-                                    thread.thread_state = ThreadState::RUNNING;
-
-                                    // Changing the thread state for the join handle.
-                                    if let Some(o) = &mut thread.output {
-                                        o.change_state(ThreadState::RUNNING);
-                                    }
-
-                                    // Changing the current stack pointer to the thread's ones.
-                                    stack_frame.stack_ptr = thread.stack_ptr.load(Ordering::Acquire);
-                                    stack_frame.instruction_pointer = thread.instruction_ptr.load(Ordering::Acquire);
-                                },
-                                ThreadState::FINAL => (break),
-                                _ => (),
-                            }
-
-                            // Changing the current stack pointer to the thread's ones.
-                            stack_frame.stack_ptr = thread.stack_ptr.load(Ordering::Acquire);
-                            stack_frame.instruction_pointer = thread.instruction_ptr.load(Ordering::Acquire);
-                            break;
-                        } else {
-                            // If there are no underlying threads we must delete the hangling task
-                            ROUND_ROBIN.delete(*task);
-                        }
-                    } else {
-                        // If there are no underlying process, we must delete the hangling task
-                        ROUND_ROBIN.delete(*task);
-                    }
-                }    
-            }
-        });
-
-        PROGRAMMABLE_INTERRUPT_CONTROLLER.lock().master.end_of_interrupt();
-
-        // Before the iretq instruction is done, we must change the rdi, so it can be used as
-        // a pointer parameter for a thread function. Because the calling convention automatically
-        // generates a code that pops the rdi, own epilogue must be created, to prevent this action.
-        asm!(
-            "pop rdi",
-
-            "cmp {0:r}, 0x0",    // If the thread must obtain some inputs, do:
-            "cmovne rdi, {0:r}", // This line only changes the behavior.
-            
-            "add rsp, 0xa0",
-            "pop rax",
-            "pop rcx",
-            "pop rdx",
-            "pop rsi",
-            "pop r8",
-            "pop r9",
-            "pop r10",
-            "pop r11",
-            "iretq",
-            in(reg) thread_input,
-        );
-    }
-
-    /// A helper function for calling the new thread, with fast-call calling convention
-    ///
-    /// This function calls the inner function of the thread and passes the thread itself
-    /// as a mutable reference argument. This function must be divergent, because we are never
-    /// calling it ourselves but only jumping to it's address.
-    pub(crate) unsafe fn task_switch_call(t: &mut Thread) -> ! {
-        use crate::kernel_components::task_virtualization::{PriorityScheduler, Task, JoinHandle};
-        use core::mem;
-
-        let closure = t.fun.as_ref();
-
-        // Running the closure of the thread.
-        let output = closure(
-            mem::transmute(
-                t as *const _ as usize
-            )
-        );
-
-        critical_section!(|| {
-            // Marking the thread as done executing.
-            t.thread_state = ThreadState::FINAL;
-
-            // Writing the data to handle and removing the writer.
-            if let Some(o) = &mut t.output {
-                // Writing data
-                o.write(output);
-                // Changing the status
-                o.change_state(ThreadState::FINAL);
-                // Dropping the writer reference and living None on it's place.
-                drop(t.output.take());
-            }
-
-            // The PC will get here once the task is done. At this moment the task is
-            // not needed anymore and can be removed.
-            ROUND_ROBIN.delete(
-              Task { pid: t.pid, tid: t.tid }
-            );
-
-            // Trying to cleanup the process.
-            PROCESS_MANAGEMENT_UNIT.remove(t.pid);
-        });
-
-        loop {
-            interrupt::wait_for_interrupt();
-        }
-    }
-
-    /// Software keyboard interrupt handler
-    ///
-    /// This handler reads the data from the keyboard key that is pressed and puts it in a VGA
-    /// buffer. TODO! Abstract the keyboard handling and change the output buffer for the keyboard
-    /// controlls with other aplications in some queue style.
-    #[no_mangle]
-    unsafe extern "x86-interrupt" fn keyboard_interrupt_handler(stack_frame: InterruptStackFrame) {
-        use crate::kernel_components::arch_x86_64::interrupts;
-        use crate::kernel_components::drivers::{DriverType, keyboards::KeyboardDriver};
-        
-        critical_section!(|| {
-            if let Some(keyboard) = unsafe{DRIVER_MANAGER.driver::<Box<dyn KeyboardDriver>>(DriverType::Keyboard)} {
-                if let Some(key) = keyboard.read() {
-                    print!("{}", key);
-                }
-            } else {
-                warn!("Keyboard input detected, yet ignored due to no available keyboard driver found.");
-                // This allows PIC to send more keyboard interrupts.
-                let _ = PS2::new().read_data();
-            }
-        });
-        PROGRAMMABLE_INTERRUPT_CONTROLLER.lock().master.end_of_interrupt();
-    }
-
-    /// A timer interrupt handler.
-    /// 
-    /// This handler will be used to switch between different threads and make the
-    /// virtualization part of the OS possible. The timer interrupt is essential in
-    /// performing task scheduling, time sharing, event handling and power management.
-    /// 
-    /// # Warn
-    /// 
-    /// Remap the PIC controller to work properly.
-    pub const TIMER_INTERRUPT: HandlerFunction = timer_interrupt_handler;
-
-    /// A keyboard interrupt handler.
-    /// 
-    /// This handler reads the value written in data port of the PS/2 controller, which will
-    /// decode the received scancode and write it into the VGA buffer.
-    /// 
-    /// When writing your own interrupt handler for PS/2 keyboard, do not forget to read the
-    /// scancode from the data por of the PS/2 controller.
-    pub const KEYBOARD_INTERRUPT: HandlerFunction = keyboard_interrupt_handler;
-}
 
 /// Represents the interrupt stack frame pushed by the CPU on interrupt or exception entry.
 /// 
@@ -533,5 +237,3 @@ impl SelectorErrorCode {
         self.bits() == 0
     }
 }
-
-
