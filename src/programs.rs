1dcb1aad855e98d07afb62ce7b66bcf14f880350
not-forest <notforest69@gmail.com>
2025-01-17 22:05:13 +0100

    Improved the shell program to obtain proper keys from the keyboard driver. Parsing the 'Backspace' key only for now

--- src/programs.rs
+++ src/programs.rs
@@ -4,9 +4,30 @@ pub use shell::shell;
 
 /// Basic notOS shell module.
 pub mod shell {
-    use alloc::sync::Arc;
+    use alloc::vec::Vec;
+    use alloc::{sync::Arc, string::String};
 
-    use crate::{kernel_components::{keyboard_interface::KeyboardInterface, task_virtualization::Thread}, print};
+    use crate::{debug, print, println, Color};
+    use crate::kernel_components::{
+        arch_x86_64::interrupts::interrupt, 
+        keyboard_interface::KeyboardInterface, 
+        task_virtualization::Thread,
+        drivers::keyboards::Key,
+        sync::Mutex
+    };
+
+    fn shell_greetings() {
+        println!(Color::GREEN; concat!("\n\n\n", 
+                                       "# Welcome to \"notOS\" shell.\n",
+                                       "# This software is an internal part of notOS kernel.\n",
+                                       "#\n",
+                                       "# THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED", 
+                                       "INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR", 
+                                       "PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE",
+                                       "FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,",
+                                       "ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.",
+                                       "\n\n\n"))
+    }
 
     /// Small shell program that allows to write commands and receive output. 
     ///
@@ -15,10 +36,34 @@ pub mod shell {
     pub fn shell(t: &mut Thread) {
         // Creating a keyboard interface to communicate with kernel buffer.
         let mut k_interface = KeyboardInterface::new();
+        let mut shell_ptr = 0u8;
+        let mut shell_buf = Arc::new(Mutex::new(Vec::<char>::with_capacity(256))); // TODO! Swap for user variable.
 
+        shell_greetings();
+        let shell_arc = shell_buf.clone();
         // Providing one of the handlers for click event.
-        k_interface.on_click(t, |_, c| c.map(|c| print!("{}", c)));
+        k_interface.on_click(t, move |_, c| c.map(|c| {
+            let shell_buf = shell_arc.clone();
+            match c.key.key {
+                Key::Backspace => if c.key.is_pressed() { shell_buf.lock().push('\x7e') }, // Forces to clean one char
+                _ => {
+                    match c.chr {
+                        Some(chr) => shell_buf.lock().push(chr),
+                        None => ()
+                    }
+                }
+            }
+        }));
 
-        loop {}
+        loop {
+            while shell_ptr < shell_buf.lock().len() as u8 {
+                shell_buf.lock()
+                    .last()
+                    .map(|c| print!("{}", c));
+                shell_ptr += 1;
+            }
+
+            interrupt::wait_for_interrupt();
+        }
     }
 }
