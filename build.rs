56c88daa6e3c269813f257eb9d5fb1bc0d87f6c4
not-forest <notforest69@gmail.com>
2026-05-26 23:06:25 +0200

    [1] Clear reinitialization of the project structure.

--- build.rs
+++
@@ -1,7 +0,0 @@
-use std::env;
-
-fn main() {
-    let current_dir = env::current_dir().expect("Failed to get current directory");
-    println!("cargo:rustc-link-search=native={}/build", current_dir.as_path().to_string_lossy());
-    println!("cargo:rustc-link-arg=-T{}/src/arch/x86_64/linker.ld", current_dir.as_path().to_string_lossy());
-}
